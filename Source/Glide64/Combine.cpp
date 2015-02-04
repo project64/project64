c09e04e7fa898033806341a08a1bdaea6c11292a
unknown <rj150@hotmail.com>
2015-02-04 15:44:56 -0500

    (Glide64) ammend previous fixes to treat undefined cases as errors

--- Source/Glide64/Combine.cpp
+++ Source/Glide64/Combine.cpp
@@ -15612,7 +15612,7 @@ void Combine ()
     current = left + ((right-left) >> 1);
     if (current == last)
     {
-      FRDP("Warning!  Could not initialize current_combiner to valid index.\n");
+      FRDP_E("Could not initialize current_combiner to valid index.\n");
       current_combine = ~0u; /* Debug uncertain case, and fix warning (cxd4). */
       break;  // can't be found!
     }
@@ -16018,7 +16018,7 @@ void ColorCombinerToExtension ()
     ext_local_a = GR_CMBX_ITALPHA;
     break;
   default:
-    FRDP("Invalid combiner locality %u.\n", cmb.c_loc);
+    FRDP_E("Invalid combiner locality %u.\n", cmb.c_loc);
   case GR_COMBINE_LOCAL_CONSTANT:
     ext_local = GR_CMBX_CONSTANT_COLOR;
     ext_local_a = GR_CMBX_CONSTANT_ALPHA;
@@ -16035,7 +16035,7 @@ void ColorCombinerToExtension ()
     ext_other_a = GR_CMBX_TEXTURE_ALPHA;
     break;
   default:
-    FRDP("Invalid combiner flag %u.\n", cmb.c_oth);
+    FRDP_E("Invalid combiner flag %u.\n", cmb.c_oth);
   case GR_COMBINE_OTHER_CONSTANT:
     ext_other = GR_CMBX_CONSTANT_COLOR;
     ext_other_a = GR_CMBX_CONSTANT_ALPHA;
@@ -16382,7 +16382,7 @@ void TexColorCombinerToExtension (GrChipID_t tmu)
     tc_ext_c_invert = 1;
     break;
   default:
-    FRDP("Invalid combiner TMU factor %u.\n", tmu_fac);
+    FRDP_E("Invalid combiner TMU factor %u.\n", tmu_fac);
   case GR_COMBINE_FACTOR_ONE_MINUS_DETAIL_FACTOR:
     tc_ext_c = GR_CMBX_DETAIL_FACTOR;
     tc_ext_c_invert = 1;
@@ -16478,7 +16478,7 @@ void TexColorCombinerToExtension (GrChipID_t tmu)
     tc_ext_d_invert = 0;
     break;
   default:
-    FRDP("Invalid combiner TMU function %u.\n", tmu_func);
+    FRDP_E("Invalid combiner TMU function %u.\n", tmu_func);
   case GR_COMBINE_FUNCTION_SCALE_MINUS_LOCAL_ADD_LOCAL_ALPHA:
     tc_ext_a = GR_CMBX_LOCAL_TEXTURE_RGB;
     tc_ext_a_mode = GR_FUNC_MODE_ZERO;
@@ -16563,7 +16563,7 @@ void TexAlphaCombinerToExtension (GrChipID_t tmu)
     ta_ext_c_invert = 1;
     break;
   default:
-    FRDP("Invalid combiner alpha factor %u.\n", tmu_a_fac);
+    FRDP_E("Invalid combiner alpha factor %u.\n", tmu_a_fac);
   case GR_COMBINE_FACTOR_ONE_MINUS_DETAIL_FACTOR:
     ta_ext_c = GR_CMBX_DETAIL_FACTOR;
     ta_ext_c_invert = 1;
@@ -16628,7 +16628,7 @@ void TexAlphaCombinerToExtension (GrChipID_t tmu)
     ta_ext_d_invert = 0;
     break;
   default:
-    FRDP("Invalid combiner alpha coverage function %u.\n", tmu_a_func);
+    FRDP_E("Invalid combiner alpha coverage function %u.\n", tmu_a_func);
   case GR_COMBINE_FUNCTION_SCALE_MINUS_LOCAL_ADD_LOCAL:
   case GR_COMBINE_FUNCTION_SCALE_MINUS_LOCAL_ADD_LOCAL_ALPHA:
     ta_ext_a = GR_CMBX_LOCAL_TEXTURE_ALPHA;
