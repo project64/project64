436888dd6bf5597834aacf5f26ef8238e901e0da
zilmar <zilmar@pj64-emu.com>
2015-02-19 19:03:33 +1100

    Update version numbers and create a package zip

--- Source/RSP/Version.h
+++ Source/RSP/Version.h
@@ -29,7 +29,7 @@
 #define VERSION_MAJOR               1
 #define VERSION_MINOR               7
 #define VERSION_REVISION            0
-#define VERSION_BUILD               9
+#define VERSION_BUILD               10
 
 #define VER_FILE_DESCRIPTION_STR    "RSP emulation Plugin"
 #define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD
