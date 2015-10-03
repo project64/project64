118134766c681a48702a9cd7019989bdc9df6eeb
Nekokabu <nekokabu@gmail.com>
2015-10-03 13:35:27 +0900

    fix character corruption
    Add setlocale

--- Source/Project64/main.cpp
+++ Source/Project64/main.cpp
@@ -198,6 +198,11 @@ int WINAPI WinMain(HINSTANCE /*hInstance*/, HINSTANCE /*hPrevInstance*/, LPSTR /
 {
 	FixDirectories();
 
+	char *lbuffer = new char[10];
+	if (GetLocaleInfoA(LOCALE_SYSTEM_DEFAULT, LOCALE_SABBREVLANGNAME, lbuffer, 10))
+		setlocale(LC_ALL, lbuffer);
+	delete lbuffer;
+
 	CoInitialize(NULL);
 	try
 	{
