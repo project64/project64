9d35f6cf23d340c08ee522ede7386dcd87bb7dc5
zilmar <zilmar@pj64-emu.com>
2015-10-09 10:10:26 +1100

    [Glide64] Remove the broken build-scripts/workspaces
    https://github.com/mupen64plus/mupen64plus-video-glide64mk2/commit/58d74825f464fb626bd63db101ef3703261a3756

--- Source/Glide64/build_asm.bat
+++
@@ -1,3 +0,0 @@
-nasm.exe -o lib\Texture.obj -O6 -fwin32 -D__WIN32__ --prefix _ Texture.asm
-nasm.exe -o lib\FixedPoint.obj -O6 -fwin32 -D__WIN32__ --prefix _ FixedPoint.asm
-nasm.exe -o lib\3dmathSIMD.obj -O6 -fwin32 -D__WIN32__ --prefix _ 3dmathSIMD.asm
