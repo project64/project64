d6db9362d71e8ee9ac6b5e5f339eec3c35aeb607
zilmar <zilmar@pj64-emu.com>
2015-11-09 19:32:13 +1100

    [Project64] change exit info to a struct instead of a class

--- Source/Project64/N64 System/Recompiler/Exit Info.h
+++ Source/Project64/N64 System/Recompiler/Exit Info.h
@@ -10,24 +10,23 @@
 ****************************************************************************/
 #pragma once
 
-class CExitInfo
+struct CExitInfo
 {
-public:
 	enum EXIT_REASON
 	{
-		Normal					= 0,
-		Normal_NoSysCheck		= 1,
-		DoCPU_Action			= 2,
-		COP1_Unuseable			= 3,
-		DoSysCall				= 4,
-		TLBReadMiss				= 5,
-		TLBWriteMiss			= 6,
-		DivByZero				= 7,
-		ExitResetRecompCode		= 8,
+		Normal = 0,
+		Normal_NoSysCheck = 1,
+		DoCPU_Action = 2,
+		COP1_Unuseable = 3,
+		DoSysCall = 4,
+		TLBReadMiss = 5,
+		TLBWriteMiss = 6,
+		DivByZero = 7,
+		ExitResetRecompCode = 8,
 	};
 
-	DWORD       ID;
-	DWORD       TargetPC;
+	uint32_t    ID;
+	uint32_t    TargetPC;
 	CRegInfo    ExitRegSet;
 	EXIT_REASON reason;
 	STEP_TYPE   NextInstruction;
