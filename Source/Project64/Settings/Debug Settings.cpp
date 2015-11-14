68513c11016a337cd327824b170c29e1837f9de3
zilmar <zilmar@pj64-emu.com>
2015-11-15 08:32:57 +1100

    [Project64] Cleanup Debug Settings.cpp

--- Source/Project64/Settings/Debug Settings.cpp
+++ Source/Project64/Settings/Debug Settings.cpp
@@ -10,7 +10,7 @@
 ****************************************************************************/
 #include "stdafx.h"
 
-int CDebugSettings::m_RefCount = 0; 
+int CDebugSettings::m_RefCount = 0;
 
 bool CDebugSettings::m_bHaveDebugger = false;
 bool CDebugSettings::m_bLogX86Code = false;
@@ -20,35 +20,35 @@ bool CDebugSettings::m_Registered = false;
 
 CDebugSettings::CDebugSettings()
 {
-	m_RefCount += 1;
-	if (!m_Registered && g_Settings)
-	{
-		m_Registered = true;
-		g_Settings->RegisterChangeCB(Debugger_Enabled,this,(CSettings::SettingChangedFunc)StaticRefreshSettings);
-		g_Settings->RegisterChangeCB(Debugger_GenerateLogFiles,this,(CSettings::SettingChangedFunc)StaticRefreshSettings);
-		g_Settings->RegisterChangeCB(Debugger_ShowTLBMisses,this,(CSettings::SettingChangedFunc)StaticRefreshSettings);
-		g_Settings->RegisterChangeCB(Debugger_ShowDivByZero,this,(CSettings::SettingChangedFunc)StaticRefreshSettings);
-		
-		RefreshSettings();
-	}
+    m_RefCount += 1;
+    if (!m_Registered && g_Settings)
+    {
+        m_Registered = true;
+        g_Settings->RegisterChangeCB(Debugger_Enabled, this, (CSettings::SettingChangedFunc)StaticRefreshSettings);
+        g_Settings->RegisterChangeCB(Debugger_GenerateLogFiles, this, (CSettings::SettingChangedFunc)StaticRefreshSettings);
+        g_Settings->RegisterChangeCB(Debugger_ShowTLBMisses, this, (CSettings::SettingChangedFunc)StaticRefreshSettings);
+        g_Settings->RegisterChangeCB(Debugger_ShowDivByZero, this, (CSettings::SettingChangedFunc)StaticRefreshSettings);
+
+        RefreshSettings();
+    }
 }
 
 CDebugSettings::~CDebugSettings()
 {
-	m_RefCount -= 1;
-	if (m_RefCount == 0 && g_Settings)
-	{
-		g_Settings->UnregisterChangeCB(Debugger_Enabled,this,(CSettings::SettingChangedFunc)StaticRefreshSettings);
-		g_Settings->UnregisterChangeCB(Debugger_GenerateLogFiles,this,(CSettings::SettingChangedFunc)StaticRefreshSettings);
-		g_Settings->UnregisterChangeCB(Debugger_ShowTLBMisses,this,(CSettings::SettingChangedFunc)StaticRefreshSettings);
-		g_Settings->UnregisterChangeCB(Debugger_ShowDivByZero,this,(CSettings::SettingChangedFunc)StaticRefreshSettings);
-	}
+    m_RefCount -= 1;
+    if (m_RefCount == 0 && g_Settings)
+    {
+        g_Settings->UnregisterChangeCB(Debugger_Enabled, this, (CSettings::SettingChangedFunc)StaticRefreshSettings);
+        g_Settings->UnregisterChangeCB(Debugger_GenerateLogFiles, this, (CSettings::SettingChangedFunc)StaticRefreshSettings);
+        g_Settings->UnregisterChangeCB(Debugger_ShowTLBMisses, this, (CSettings::SettingChangedFunc)StaticRefreshSettings);
+        g_Settings->UnregisterChangeCB(Debugger_ShowDivByZero, this, (CSettings::SettingChangedFunc)StaticRefreshSettings);
+    }
 }
 
 void CDebugSettings::RefreshSettings()
 {
-	m_bHaveDebugger = g_Settings->LoadBool(Debugger_Enabled);
-	m_bLogX86Code = m_bHaveDebugger && g_Settings->LoadBool(Debugger_GenerateLogFiles);
-	m_bShowTLBMisses = m_bHaveDebugger && g_Settings->LoadBool(Debugger_ShowTLBMisses);
-	m_bShowDivByZero = m_bHaveDebugger && g_Settings->LoadBool(Debugger_ShowDivByZero);
+    m_bHaveDebugger = g_Settings->LoadBool(Debugger_Enabled);
+    m_bLogX86Code = m_bHaveDebugger && g_Settings->LoadBool(Debugger_GenerateLogFiles);
+    m_bShowTLBMisses = m_bHaveDebugger && g_Settings->LoadBool(Debugger_ShowTLBMisses);
+    m_bShowDivByZero = m_bHaveDebugger && g_Settings->LoadBool(Debugger_ShowDivByZero);
 }
