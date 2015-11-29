bc8d8b8fa10203892be339b1e173ead29936df62
zilmar <zilmar@pj64-emu.com>
2015-11-30 06:21:47 +1100

    [Projct64] Clean up std string.cpp

--- Source/Common/std string.h
+++ Source/Common/std string.h
@@ -9,63 +9,62 @@ class stdstr;
 
 typedef std::vector<stdstr> strvector;
 
-class stdstr : 
-	public std::string
+class stdstr :
+    public std::string
 {
 public:
-	stdstr();
-	stdstr( const std::string & str );
-	stdstr( const stdstr & str );
-	stdstr( const char * str );
+    stdstr();
+    stdstr(const std::string & str);
+    stdstr(const stdstr & str);
+    stdstr(const char * str);
 
-	strvector  Tokenize ( char delimiter ) const;
-	strvector  Tokenize ( const char * delimiter ) const;
-	void       Format   ( const char * strFormat, ... );
-	stdstr&    ToLower  ( void );
-	stdstr&    ToUpper  ( void );
+    strvector  Tokenize(char delimiter) const;
+    strvector  Tokenize(const char * delimiter) const;
+    void       Format(const char * strFormat, ...);
+    stdstr&    ToLower(void);
+    stdstr&    ToUpper(void);
 
-	void       Replace  ( const char search, const char replace );
-	void       Replace  ( const char * search, const char replace );
-	void       Replace  ( const std::string & search, const std::string & replace );
+    void       Replace(const char search, const char replace);
+    void       Replace(const char * search, const char replace);
+    void       Replace(const std::string & search, const std::string & replace);
 
-	stdstr   & Trim      ( const char * chars2remove = "\t " );
-	stdstr   & TrimLeft  ( const char * chars2remove = "\t " );
-	stdstr   & TrimRight ( const char * chars2remove = "\t " );
+    stdstr   & Trim(const char * chars2remove = "\t ");
+    stdstr   & TrimLeft(const char * chars2remove = "\t ");
+    stdstr   & TrimRight(const char * chars2remove = "\t ");
 
-	stdstr   & FromUTF16 ( const wchar_t * UTF16Source, bool * bSuccess = NULL);
-	std::wstring  ToUTF16 ( bool * bSuccess = NULL);
+    stdstr   & FromUTF16(const wchar_t * UTF16Source, bool * bSuccess = NULL);
+    std::wstring  ToUTF16(bool * bSuccess = NULL);
 
-	void ArgFormat( const char * strFormat, va_list & args);
+    void ArgFormat(const char * strFormat, va_list & args);
+};
 
-}; 
-
-class stdstr_f: public stdstr 
+class stdstr_f : public stdstr
 {
 public:
-	stdstr_f(const char * strFormat, ...) 
-	{ 
-		va_list args;
-		va_start(args, strFormat);
-		ArgFormat(strFormat,args);
-		va_end(args);
-	}
+    stdstr_f(const char * strFormat, ...)
+    {
+        va_list args;
+        va_start(args, strFormat);
+        ArgFormat(strFormat, args);
+        va_end(args);
+    }
 };
 
 class stdwstr_f : public std::wstring
 {
 public:
-	stdwstr_f(const wchar_t * strFormat, ...)
-	{
-		va_list args;
-		va_start(args, strFormat);
+    stdwstr_f(const wchar_t * strFormat, ...)
+    {
+        va_list args;
+        va_start(args, strFormat);
 
-		wchar_t Msg[1000];
-		_vsnwprintf(Msg, sizeof(Msg) - 1, strFormat, args);
+        wchar_t Msg[1000];
+        _vsnwprintf(Msg, sizeof(Msg) - 1, strFormat, args);
 
-		va_end(args);
+        va_end(args);
 
-		this->assign(Msg);
-	}
+        this->assign(Msg);
+    }
 };
 
 typedef std::list<stdstr>   strlist;
