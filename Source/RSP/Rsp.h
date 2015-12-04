f7b67671496bbca471e339679a166e4b9c237a27
no <rj150@hotmail.com>
2015-12-04 17:24:15 -0500

    [RSP] Start using portable types in plugin header.

--- Source/RSP/Rsp.h
+++ Source/RSP/Rsp.h
@@ -28,6 +28,8 @@
 extern "C" {
 #endif
 
+#include <Common/stdtypes.h>
+
 #if defined(_WIN32)
 #define EXPORT          __declspec(dllexport)
 #define CALL            _cdecl
@@ -50,45 +52,45 @@ extern "C" {
 #define PLUGIN_TYPE_CONTROLLER		4
 
 typedef struct {
-	WORD Version;        /* Should be set to 0x0101 */
-	WORD Type;           /* Set to PLUGIN_TYPE_RSP */
+    uint16_t Version;        /* Should be set to 0x0101 */
+    uint16_t Type;           /* Set to PLUGIN_TYPE_RSP */
 	char Name[100];      /* Name of the DLL */
 
 	/* If DLL supports memory these memory options then set them to TRUE or FALSE
 	   if it does not support it */
-	BOOL NormalMemory;   /* a normal BYTE array */ 
-	BOOL MemoryBswaped;  /* a normal BYTE array where the memory has been pre
-	                          bswap on a dword (32 bits) boundry */
+    int NormalMemory;   /* a normal BYTE array */ 
+    int MemoryBswaped;  /* a normal BYTE array where the memory has been pre
+                              bswap on a dword (32 bits) boundry */
 } PLUGIN_INFO;
 
 typedef struct {
 	HINSTANCE hInst;
-	BOOL MemoryBswaped;    /* If this is set to TRUE, then the memory has been pre
-	                          bswap on a dword (32 bits) boundry */
-	BYTE * RDRAM;
-	BYTE * DMEM;
-	BYTE * IMEM;
+    int MemoryBswaped;    /* If this is set to TRUE, then the memory has been pre
+                              bswap on a dword (32 bits) boundry */
+    uint8_t * RDRAM;
+    uint8_t * DMEM;
+    uint8_t * IMEM;
 
-	DWORD * MI_INTR_REG;
+    uint32_t * MI_INTR_REG;
 
-	DWORD * SP_MEM_ADDR_REG;
-	DWORD * SP_DRAM_ADDR_REG;
-	DWORD * SP_RD_LEN_REG;
-	DWORD * SP_WR_LEN_REG;
-	DWORD * SP_STATUS_REG;
-	DWORD * SP_DMA_FULL_REG;
-	DWORD * SP_DMA_BUSY_REG;
-	DWORD * SP_PC_REG;
-	DWORD * SP_SEMAPHORE_REG;
+    uint32_t * SP_MEM_ADDR_REG;
+    uint32_t * SP_DRAM_ADDR_REG;
+    uint32_t * SP_RD_LEN_REG;
+    uint32_t * SP_WR_LEN_REG;
+    uint32_t * SP_STATUS_REG;
+    uint32_t * SP_DMA_FULL_REG;
+    uint32_t * SP_DMA_BUSY_REG;
+    uint32_t * SP_PC_REG;
+    uint32_t * SP_SEMAPHORE_REG;
 
-	DWORD * DPC_START_REG;
-	DWORD * DPC_END_REG;
-	DWORD * DPC_CURRENT_REG;
-	DWORD * DPC_STATUS_REG;
-	DWORD * DPC_CLOCK_REG;
-	DWORD * DPC_BUFBUSY_REG;
-	DWORD * DPC_PIPEBUSY_REG;
-	DWORD * DPC_TMEM_REG;
+    uint32_t * DPC_START_REG;
+    uint32_t * DPC_END_REG;
+    uint32_t * DPC_CURRENT_REG;
+    uint32_t * DPC_STATUS_REG;
+    uint32_t * DPC_CLOCK_REG;
+    uint32_t * DPC_BUFBUSY_REG;
+    uint32_t * DPC_PIPEBUSY_REG;
+    uint32_t * DPC_TMEM_REG;
 
 	void (*CheckInterrupts)( void );
 	void (*ProcessDList)( void );
@@ -104,7 +106,7 @@ typedef struct {
 	void (*ProcessMenuItem) ( int ID );
 
 	/* Break Points */
-	BOOL UseBPoints;
+    int UseBPoints;
 	char BPPanelName[20];
 	void (*Add_BPoint)      ( void );
 	void (*CreateBPPanel)   ( HWND hDlg, RECT rcBox );
@@ -150,7 +152,7 @@ int  GetStoredWinPos( char * WinName, DWORD * X, DWORD * Y );
 #define InterpreterCPU	0
 #define RecompilerCPU	1
 
-extern BOOL DebuggingEnabled, Profiling, IndvidualBlock, ShowErrors, BreakOnStart, LogRDP, LogX86Code;
+extern int DebuggingEnabled, Profiling, IndvidualBlock, ShowErrors, BreakOnStart, LogRDP, LogX86Code;
 extern DWORD CPUCore;
 extern DEBUG_INFO DebugInfo;
 extern RSP_INFO RSPInfo;
