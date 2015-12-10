ac1211eb7681db2c2d234f70295564f15443b164
zilmar <zilmar@pj64-emu.com>
2015-12-10 17:38:33 +1100

    [Project64] Cleanup Trace Files

--- Source/Common/Trace.h
+++ Source/Common/Trace.h
@@ -8,12 +8,12 @@ class CTraceModule
     TraceLevel m_Type;
 
 public:
-    CTraceModule () {  m_Type = TrLvError; }
-    virtual ~CTraceModule () {}
+    CTraceModule() { m_Type = TrLvError; }
+    virtual ~CTraceModule() {}
 
-    inline  void       SetTraceLevel ( TraceLevel Type ) { m_Type = Type; }
-    inline  TraceLevel GetTraceLevel ( void ) const     { return m_Type; }
-    virtual void       Write         ( const char * Message, bool EndOfLine ) = 0;
+    inline  void       SetTraceLevel(TraceLevel Type) { m_Type = Type; }
+    inline  TraceLevel GetTraceLevel(void) const     { return m_Type; }
+    virtual void       Write(const char * Message, bool EndOfLine) = 0;
 };
 
 class CTraceFileLog : public CTraceModule
@@ -25,19 +25,19 @@ class CTraceFileLog : public CTraceModule
     bool            m_FlushFile;
 
 public:
-    CTraceFileLog (const char * FileName, bool FlushFile = true);
+    CTraceFileLog(const char * FileName, bool FlushFile = true);
     CTraceFileLog(const char * FileName, bool FlushFile, LOG_OPEN_MODE eMode, size_t dwMaxFileSize = 5);
-    virtual ~CTraceFileLog ();
+    virtual ~CTraceFileLog();
 
-    void Write ( const char * Message, bool EndOfLine );
-    void SetFlushFile ( bool bFlushFile );
+    void Write(const char * Message, bool EndOfLine);
+    void SetFlushFile(bool bFlushFile);
 };
 
 class CDebugTraceLog : public CTraceModule
 {
 public:
-    void Write ( const char * Message, bool EndOfLine );
+    void Write(const char * Message, bool EndOfLine);
 };
 
-CTraceModule * AddTraceModule    ( CTraceModule * TraceModule ); // Must be created with new
-CTraceModule * RemoveTraceModule ( CTraceModule * TraceModule ); // Is not automaticly deleted
+CTraceModule * AddTraceModule(CTraceModule * TraceModule); // Must be created with new
+CTraceModule * RemoveTraceModule(CTraceModule * TraceModule); // Is not automaticly deleted
