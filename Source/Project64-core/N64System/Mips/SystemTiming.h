199fa2f7f8da46e8b6fe476ba29b6683ecc3a679
zilmar <zilmar@pj64-emu.com>
2015-12-13 17:58:08 +1100

    [Project64] Clean up SystemTiming.cpp

--- Source/Project64-core/N64System/Mips/SystemTiming.h
+++ Source/Project64-core/N64System/Mips/SystemTiming.h
@@ -16,56 +16,56 @@
 class CSystemTimer
 {
 public:
-	enum TimerType
-	{
-		UnknownTimer, 
-		CompareTimer, 
-		SoftResetTimer, 
-		ViTimer, 
-		AiTimerInterrupt, 
-		AiTimerBusy, 
-		AiTimerDMA, 
-		SiTimer, 
-		PiTimer, 
-		RspTimer, 
-		RSPTimerDlist, 
-		MaxTimer
-	};
+    enum TimerType
+    {
+        UnknownTimer,
+        CompareTimer,
+        SoftResetTimer,
+        ViTimer,
+        AiTimerInterrupt,
+        AiTimerBusy,
+        AiTimerDMA,
+        SiTimer,
+        PiTimer,
+        RspTimer,
+        RSPTimerDlist,
+        MaxTimer
+    };
 
-	struct TIMER_DETAILS
-	{
-		bool    Active;
-		__int64 CyclesToTimer;
-	};
+    struct TIMER_DETAILS
+    {
+        bool    Active;
+        __int64 CyclesToTimer;
+    };
 
 public:
-	          CSystemTimer         ( int & NextTimer );
+    CSystemTimer(int & NextTimer);
     void      SetTimer(TimerType Type, uint32_t Cycles, bool bRelative);
     uint32_t  GetTimer(TimerType Type);
-	void      StopTimer            ( TimerType Type );
-	void      UpdateTimers         ();
-	void      TimerDone            ();
-	void      Reset                ();
-	void      UpdateCompareTimer   ();
-	bool      SaveAllowed          ();
-	
-	void      SaveData             ( void * file ) const;
-	void      LoadData             ( void * file );
+    void      StopTimer(TimerType Type);
+    void      UpdateTimers();
+    void      TimerDone();
+    void      Reset();
+    void      UpdateCompareTimer();
+    bool      SaveAllowed();
 
-	void RecordDifference( CLog &LogFile, const CSystemTimer& rSystemTimer);
+    void      SaveData(void * file) const;
+    void      LoadData(void * file);
 
-	TimerType CurrentType() const { return m_Current; }
+    void RecordDifference(CLog &LogFile, const CSystemTimer& rSystemTimer);
 
-	bool operator == (const CSystemTimer& rSystemTimer) const;
-	bool operator != (const CSystemTimer& rSystemTimer) const;
+    TimerType CurrentType() const { return m_Current; }
+
+    bool operator == (const CSystemTimer& rSystemTimer) const;
+    bool operator != (const CSystemTimer& rSystemTimer) const;
 
 private:
-	TIMER_DETAILS m_TimerDetatils[MaxTimer];
-	int           m_LastUpdate; //Timer at last update
-	int         & m_NextTimer;
-	TimerType     m_Current;
-	bool          m_inFixTimer;
+    TIMER_DETAILS m_TimerDetatils[MaxTimer];
+    int           m_LastUpdate; //Timer at last update
+    int         & m_NextTimer;
+    TimerType     m_Current;
+    bool          m_inFixTimer;
 
-	void SetCompareTimer();
-	void FixTimers();
+    void SetCompareTimer();
+    void FixTimers();
 };
