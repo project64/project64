c49258420af91904f2be6c12b12e452848b83a8a
no <rj150@hotmail.com>
2015-12-21 15:34:48 -0500

    undeclared memset()

--- Source/Project64-core/N64System/Mips/PifRam.cpp
+++ Source/Project64-core/N64System/Mips/PifRam.cpp
@@ -9,7 +9,9 @@
 *                                                                           *
 ****************************************************************************/
 #include "stdafx.h"
+#include <string.h>
 #include "PifRam.h"
+
 #include <Project64-core/N64System/SystemGlobals.h>
 #include <Project64-core/Plugins/ControllerPlugin.h>
 #include <Project64-core/N64System/Mips/RegisterClass.h>
