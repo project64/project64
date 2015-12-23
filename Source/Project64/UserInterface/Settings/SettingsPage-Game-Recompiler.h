45166a582488aabbef401cc68c12928dd344dec0
zilmar <zilmar@pj64-emu.com>
2015-12-24 06:59:14 +1100

    [Project64] Update Settings/SettingsPage-Game-Recompiler.cpp

--- Source/Project64/UserInterface/Settings/SettingsPage-Game-Recompiler.h
+++ Source/Project64/UserInterface/Settings/SettingsPage-Game-Recompiler.h
@@ -11,36 +11,35 @@
 #pragma once
 
 class CGameRecompilePage :
-	public CSettingsPageImpl<CGameRecompilePage>,
-	public CSettingsPage
+    public CSettingsPageImpl<CGameRecompilePage>,
+    public CSettingsPage
 {
+    BEGIN_MSG_MAP_EX(CGameRecompilePage)
+        COMMAND_HANDLER_EX(IDC_CPU_TYPE, LBN_SELCHANGE, ComboBoxChanged)
+        COMMAND_HANDLER_EX(IDC_FUNCFIND, LBN_SELCHANGE, ComboBoxChanged)
+        COMMAND_ID_HANDLER_EX(IDC_ROM_REGCACHE, CheckBoxChanged)
+        COMMAND_ID_HANDLER_EX(IDC_ROM_FASTSP, CheckBoxChanged)
+        COMMAND_ID_HANDLER_EX(IDC_BLOCK_LINKING, CheckBoxChanged)
+        COMMAND_ID_HANDLER_EX(IDC_SMM_CACHE, CheckBoxChanged)
+        COMMAND_ID_HANDLER_EX(IDC_SMM_DMA, CheckBoxChanged)
+        COMMAND_ID_HANDLER_EX(IDC_SMM_VALIDATE, CheckBoxChanged)
+        COMMAND_ID_HANDLER_EX(IDC_SMM_TLB, CheckBoxChanged)
+        COMMAND_ID_HANDLER_EX(IDC_SMM_PROTECT, CheckBoxChanged)
+        COMMAND_ID_HANDLER_EX(IDC_SMM_STORE, CheckBoxChanged)
+    END_MSG_MAP()
 
-	BEGIN_MSG_MAP_EX(CGameRecompilePage)
-		COMMAND_HANDLER_EX(IDC_CPU_TYPE,LBN_SELCHANGE,ComboBoxChanged)
-		COMMAND_HANDLER_EX(IDC_FUNCFIND,LBN_SELCHANGE,ComboBoxChanged)
-		COMMAND_ID_HANDLER_EX(IDC_ROM_REGCACHE,CheckBoxChanged)
-		COMMAND_ID_HANDLER_EX(IDC_ROM_FASTSP,CheckBoxChanged)
-		COMMAND_ID_HANDLER_EX(IDC_BLOCK_LINKING,CheckBoxChanged)
-		COMMAND_ID_HANDLER_EX(IDC_SMM_CACHE,CheckBoxChanged)
-		COMMAND_ID_HANDLER_EX(IDC_SMM_DMA,CheckBoxChanged)
-		COMMAND_ID_HANDLER_EX(IDC_SMM_VALIDATE,CheckBoxChanged)
-		COMMAND_ID_HANDLER_EX(IDC_SMM_TLB,CheckBoxChanged)
-		COMMAND_ID_HANDLER_EX(IDC_SMM_PROTECT,CheckBoxChanged)
-		COMMAND_ID_HANDLER_EX(IDC_SMM_STORE,CheckBoxChanged)
-	END_MSG_MAP()
-
-	enum { IDD = IDD_Settings_GameRecompiler };
+    enum { IDD = IDD_Settings_GameRecompiler };
 
 public:
-	CGameRecompilePage(HWND hParent, const RECT & rcDispay );
+    CGameRecompilePage(HWND hParent, const RECT & rcDispay);
 
-	LanguageStringID PageTitle     ( void ) { return TAB_RECOMPILER; }
-	void             HidePage      ( void );
-	void             ShowPage      ( void );
-	void             ApplySettings ( bool UpdateScreen );
-	bool             EnableReset   ( void );
-	void             ResetPage     ( void );
+    LanguageStringID PageTitle(void) { return TAB_RECOMPILER; }
+    void             HidePage(void);
+    void             ShowPage(void);
+    void             ApplySettings(bool UpdateScreen);
+    bool             EnableReset(void);
+    void             ResetPage(void);
 
 private:
-	CPartialGroupBox m_SelfModGroup;
+    CPartialGroupBox m_SelfModGroup;
 };
