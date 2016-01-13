8682d510355748846e8a189e60fa83ea29bb4dd3
zilmar <zilmar@pj64-emu.com>
2016-01-14 08:40:29 +1100

    [Project64] Remove some unneeded headers from Project64/N64System.h

--- Source/Project64/N64System.h
+++ Source/Project64/N64System.h
@@ -25,49 +25,5 @@
 #include <Project64-core/N64System/SpeedLimiterClass.h>
 #include <Project64-core/N64System/Mips/OpCode.h>
 #include <Project64-core/N64System/Recompiler/X86ops.h>
-#include <Project64-core/N64System/Mips/Mempak.h>
-#include <Project64-core/N64System/Mips/Rumblepak.h>
-#include <Project64-core/N64System/Mips/FlashRam.h>
-#include <Project64-core/N64System/Mips/Sram.h>
-#include <Project64-core/N64System/Mips/Eeprom.h>
-#include <Project64-core/N64System/Mips/Dma.h>
-#include <Project64-core/N64System/Mips/PifRam.h>
-#include <Project64-core/N64System/Mips/RegisterClass.h>
-#include <Project64-core/N64System/Mips/TranslateVaddr.h>
-#include <Project64-core/N64System/Mips/TLBClass.h>
-#include <Project64-core/N64System/Mips/MemoryLabelsClass.h>
-#include <Project64-core/N64System/Mips/MemoryVirtualMem.h>
-#include <Project64-core/N64System/Mips/Audio.h>
-#include <Project64-core/N64System/Mips/SystemTiming.h>
-#include <Project64-core/N64System/Mips/SystemEvents.h>
-
-//Interpter
-#include <Project64-core/N64System/Interpreter/InterpreterOps.h>
-#include <Project64-core/N64System/Interpreter/InterpreterOps32.h>
-#include <Project64-core/N64System/Interpreter/InterpreterCPU.h>
-
-//Recompiler
-#include <Project64-core/N64System/Recompiler/RecompilerMemory.h>
-#include <Project64-core/N64System/Recompiler/RegInfo.h>
-#include <Project64-core/N64System/Recompiler/LoopAnalysis.h>
-#include <Project64-core/N64System/Recompiler/RecompilerOps.h>
-#include <Project64-core/N64System/Mips/MemoryVirtualMem.h> //needs to inherit Recompiler ops
-#include <Project64-core/N64System/Recompiler/ExitInfo.h>
-#include <Project64-core/N64System/Recompiler/JumpInfo.h>
-#include <Project64-core/N64System/Recompiler/CodeSection.h>
-#include <Project64-core/N64System/Recompiler/CodeBlock.h>
-#include <Project64-core/N64System/Recompiler/SectionInfo.h>
-#include <Project64-core/N64System/Recompiler/FunctionInfo.h>
-#include <Project64-core/N64System/Recompiler/FunctionMapClass.h>
-#include <Project64-core/N64System/Recompiler/RecompilerClass.h>
-#include <Project64-core/N64System/Recompiler/x86CodeLog.h>
-
-//cheats
-#include <Project64-core/N64System/CheatClass.h>
-
-//Debugger
-#include "N64System/Debugger/Debugger.h"
-
-//Main Files
 #include <Project64-core/N64System/N64Class.h>
 #include <Project64-core/N64System/SystemGlobals.h>
