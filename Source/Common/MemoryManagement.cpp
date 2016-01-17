ec03eb642afe456676f8b727e036525b7d9d6fbf
zilmar <zilmar@pj64-emu.com>
2016-01-17 16:54:08 +1100

    [Common] Cleanup MemoryManagement.cpp

--- Source/Common/MemoryManagement.cpp
+++ Source/Common/MemoryManagement.cpp
@@ -2,38 +2,38 @@
 #include <windows.h>
 #include "MemoryManagement.h"
 
-static bool TranslateFromMemProtect ( MEM_PROTECTION memProtection, int & OsMemProtection)
+static bool TranslateFromMemProtect(MEM_PROTECTION memProtection, int & OsMemProtection)
 {
-	switch (memProtection)
-	{
-	case MEM_NOACCESS: OsMemProtection = PAGE_NOACCESS; break;
-	case MEM_READONLY: OsMemProtection = PAGE_READONLY; break;
-	case MEM_READWRITE: OsMemProtection = PAGE_READWRITE; break;
-	case MEM_EXECUTE_READWRITE: OsMemProtection = PAGE_EXECUTE_READWRITE; break;
-	default:
-		return false;
-	}
-	return true;
+    switch (memProtection)
+    {
+    case MEM_NOACCESS: OsMemProtection = PAGE_NOACCESS; break;
+    case MEM_READONLY: OsMemProtection = PAGE_READONLY; break;
+    case MEM_READWRITE: OsMemProtection = PAGE_READWRITE; break;
+    case MEM_EXECUTE_READWRITE: OsMemProtection = PAGE_EXECUTE_READWRITE; break;
+    default:
+        return false;
+    }
+    return true;
 }
 
 void* AllocateAddressSpace(size_t size)
 {
-	return VirtualAlloc(NULL, size, MEM_RESERVE | MEM_TOP_DOWN, PAGE_NOACCESS);
+    return VirtualAlloc(NULL, size, MEM_RESERVE | MEM_TOP_DOWN, PAGE_NOACCESS);
 }
 
 bool FreeAddressSpace(void* addr, size_t size)
 {
-	return VirtualFree(addr, 0, MEM_RELEASE) != 0;
+    return VirtualFree(addr, 0, MEM_RELEASE) != 0;
 }
 
 void* CommitMemory(void* addr, size_t size, MEM_PROTECTION memProtection)
 {
-	int OsMemProtection;
-	if (!TranslateFromMemProtect(memProtection,OsMemProtection))
-	{
-		return NULL;
-	}
-	return VirtualAlloc(addr, size, MEM_COMMIT, OsMemProtection);
+    int OsMemProtection;
+    if (!TranslateFromMemProtect(memProtection, OsMemProtection))
+    {
+        return NULL;
+    }
+    return VirtualAlloc(addr, size, MEM_COMMIT, OsMemProtection);
 }
 
 bool DecommitMemory(void* addr, size_t size)
@@ -43,16 +43,16 @@ bool DecommitMemory(void* addr, size_t size)
 
 bool ProtectMemory(void* addr, size_t size, MEM_PROTECTION memProtection, MEM_PROTECTION * OldProtect)
 {
-	int OsMemProtection;
-	if (!TranslateFromMemProtect(memProtection,OsMemProtection))
-	{
-		return NULL;
-	}
+    int OsMemProtection;
+    if (!TranslateFromMemProtect(memProtection, OsMemProtection))
+    {
+        return NULL;
+    }
 
-	DWORD OldOsProtect;
+    DWORD OldOsProtect;
     BOOL res = VirtualProtect(addr, size, OsMemProtection, &OldOsProtect);
-	if (OldProtect != NULL)
-	{
-	}
-	return res != 0;
+    if (OldProtect != NULL)
+    {
+    }
+    return res != 0;
 }
