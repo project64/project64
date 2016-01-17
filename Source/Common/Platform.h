99e6df4612ac596da6413112223e1867f6c8b5af
zilmar <zilmar@pj64-emu.com>
2016-01-17 16:58:47 +1100

    [Common] Add fesetround to Platform.cpp

--- Source/Common/Platform.h
+++ Source/Common/Platform.h
@@ -7,8 +7,17 @@
 #define _stricmp strcasecmp
 #define _strnicmp strncasecmp
 #define _snprintf snprintf
+#define _isnan isnan
 #define GetCurrentThreadId pthread_self
 
 int _vscprintf (const char * format, va_list pargs);
 
+#endif
+
+//FPU rounding code
+#ifdef _WIN32
+typedef enum { FE_TONEAREST = 0, FE_TOWARDZERO, FE_UPWARD, FE_DOWNWARD } eRoundType;
+int fesetround(int RoundType);
+#else
+#include <fenv.h>
 #endif
