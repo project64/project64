1ed5b044b3cbea4ec63bb93f11735b2cc4027e79
Nicholas <nicholas@zilsoft.com>
2016-02-24 10:06:52 +1100

    [Project64] fix issue in 2012

--- Source/Project64-core/N64System/Interpreter/InterpreterOps.cpp
+++ Source/Project64-core/N64System/Interpreter/InterpreterOps.cpp
@@ -24,24 +24,24 @@
 #if (defined(_MSC_VER) && (_MSC_VER < 1800))
 double round(double num)
 {
-	return (num - floor(num) > 0.5) ? ceil(num) : floor(num);
+    return (num - floor(num) > 0.5) ? ceil(num) : floor(num);
 }
 
 float roundf(float num)
 {
-	return (num - floorf(num) > 0.5) ? ceilf(num) : floorf(num);
+    return (num - floorf(num) > 0.5) ? ceilf(num) : floorf(num);
 }
 #endif
 
-#if (defined(_MSC_VER) && (_MSC_VER < 1700))
+#if (defined(_MSC_VER) && (_MSC_VER <= 1700))
 double trunc(double num)
 {
-	return (num < 0) ? ceil(num) : floor(num);
+    return (num < 0) ? ceil(num) : floor(num);
 }
 
 float truncf(float num)
 {
-	return (num < 0) ? ceilf(num) : floorf(num);
+    return (num < 0) ? ceilf(num) : floorf(num);
 }
 #endif
 
