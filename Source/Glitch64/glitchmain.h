0e0702fd4293a30b4877f0d0beac034483bf39fd
zilmar <zilmar@pj64-emu.com>
2016-03-03 21:10:30 +1100

    [Glitch64] change grQueryResolutionsExt(FxI32*) to grQueryResolutionsExt(int32_t*)

--- Source/Glitch64/glitchmain.h
+++ Source/Glitch64/glitchmain.h
@@ -159,7 +159,7 @@ void set_copy_shader();
 //FX_ENTRY void FX_CALL grConfigWrapperExt(HINSTANCE instance, HWND hwnd);
 FX_ENTRY void FX_CALL grConfigWrapperExt(FxI32, FxI32, FxBool, FxBool);
 FX_ENTRY GrScreenResolution_t FX_CALL grWrapperFullScreenResolutionExt(FxU32*, FxU32*);
-FX_ENTRY char ** FX_CALL grQueryResolutionsExt(FxI32*);
+FX_ENTRY char ** FX_CALL grQueryResolutionsExt(int32_t*);
 FX_ENTRY FxBool FX_CALL grKeyPressedExt(FxU32 key);
 FX_ENTRY void FX_CALL grGetGammaTableExt(FxU32, FxU32*, FxU32*, FxU32*);
 
