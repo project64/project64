project64/project64

commit 52aea8738fcec816c7946576b6cf4d3eb6fa8ee0

[Glide64] Fix up to work on android

Source/Glide64/GlideExtensions.h
@@ -44,22 +44,21 @@ typedef FxU32 GrCombineMode_t;
 
 #define GR_TEXTURE_UMA_EXT                0x06
 //wrapper specific
+#ifdef _WIN32
 FX_ENTRY void FX_CALL grConfigWrapperExt(FxI32, FxI32, FxBool, FxBool);
+#else
+FX_ENTRY void FX_CALL grConfigWrapperExt(FxI32, FxBool, FxBool);
+#endif
 FX_ENTRY GrScreenResolution_t FX_CALL grWrapperFullScreenResolutionExt(FxU32*, FxU32*);
 FX_ENTRY char ** FX_CALL grQueryResolutionsExt(int32_t*);
 FX_ENTRY FxBool FX_CALL grKeyPressedExt(FxU32 key);
 FX_ENTRY void FX_CALL grGetGammaTableExt(FxU32, FxU32*, FxU32*, FxU32*);
 
-FX_ENTRY GrContext_t FX_CALL
-	grSstWinOpenExt(
-	HWND                hWnd,
-	GrScreenResolution_t screen_resolution,
-	GrScreenRefresh_t    refresh_rate,
-	GrColorFormat_t      color_format,
-	GrOriginLocation_t   origin_location,
-	GrPixelFormat_t      pixelformat,
-	int                  nColBuffers,
-	int                  nAuxBuffers);
+#ifdef _WIN32
+FX_ENTRY GrContext_t FX_CALL grSstWinOpenExt(HWND hWnd, GrScreenResolution_t screen_resolution, GrScreenRefresh_t refresh_rate, GrColorFormat_t color_format, GrOriginLocation_t origin_location, GrPixelFormat_t pixelformat, int nColBuffers, int nAuxBuffers);
+#else
+FX_ENTRY GrContext_t FX_CALL grSstWinOpenExt(GrScreenRefresh_t refresh_rate, GrColorFormat_t color_format, GrOriginLocation_t origin_location, GrPixelFormat_t pixelformat, int nColBuffers, int nAuxBuffers);
+#endif
 
 //color combiner
 FX_ENTRY void FX_CALL

Source/Glide64/Main.cpp
@@ -281,7 +281,11 @@ void ChangeSize()
 
 void ConfigWrapper()
 {
+#ifdef _WIN32
     grConfigWrapperExt(g_settings->wrpResolution, g_settings->wrpVRAM * 1024 * 1024, g_settings->wrpFBO, g_settings->wrpAnisotropic);
+#else
+    grConfigWrapperExt(g_settings->wrpVRAM * 1024 * 1024, g_settings->wrpFBO, g_settings->wrpAnisotropic);
+#endif
 }
 
 void UseUnregisteredSetting(int /*SettingID*/)
@@ -290,20 +294,31 @@ void UseUnregisteredSetting(int /*SettingID*/)
     DebugBreak();
 #endif
 }
+#ifdef ANDROID
+extern int g_width, g_height;
+#endif
 
 void ReadSettings()
 {
     g_settings->card_id = GetSetting(Set_CardId);
+#ifdef ANDROID
+    g_settings->scr_res_x = g_settings->res_x = g_width;
+    g_settings->scr_res_y = g_settings->res_y = g_height;
+#else
     g_settings->res_data = (uint32_t)GetSetting(Set_Resolution);
     if (g_settings->res_data >= 24) g_settings->res_data = 12;
     g_settings->scr_res_x = g_settings->res_x = resolutions[g_settings->res_data][0];
     g_settings->scr_res_y = g_settings->res_y = resolutions[g_settings->res_data][1];
     g_settings->wrpResolution = GetSetting(Set_wrpResolution);
+#endif
     g_settings->vsync = GetSetting(Set_vsync);
     g_settings->ssformat = (uint8_t)GetSetting(Set_ssformat);
     g_settings->show_fps = (uint8_t)GetSetting(Set_ShowFps);
     g_settings->clock = GetSetting(Set_clock);
     g_settings->clock_24_hr = GetSetting(Set_clock_24_hr);
+#ifdef ANDROID
+    g_settings->rotate = GetSetting(Set_Rotate);
+#endif
     g_settings->advanced_options = Set_basic_mode ? !GetSystemSetting(Set_basic_mode) : 0;
     g_settings->texenh_options = GetSetting(Set_texenh_options);
     g_settings->use_hotkeys = GetSetting(Set_hotkeys);
@@ -532,6 +547,9 @@ void WriteSettings(bool saveEmulationSettings)
     SetSetting(Set_ShowFps, g_settings->show_fps);
     SetSetting(Set_clock, g_settings->clock);
     SetSetting(Set_clock_24_hr, g_settings->clock_24_hr);
+#ifdef ANDROID
+    SetSetting(Set_Rotate, g_settings->rotate);
+#endif
     //SetSetting(Set_advanced_options,g_settings->advanced_options);
     SetSetting(Set_texenh_options, g_settings->texenh_options);
 
@@ -866,6 +884,10 @@ int InitGfx()
         grGlideShutdown();
         return FALSE;
     }
+#else
+    gfx_context = grSstWinOpen(GR_REFRESH_60Hz, GR_COLORFORMAT_RGBA, GR_ORIGIN_UPPER_LEFT, 2, 1);
+    g_settings->scr_res_x = g_settings->res_x = g_width;
+    g_settings->scr_res_y = g_settings->res_y = g_height;
 #endif
 
     GfxInitDone = TRUE;
@@ -1379,7 +1401,9 @@ int CALL InitiateGFX(GFX_INFO Gfx_Info)
     ReadSettings();
     char name[21] = "DEFAULT";
     ReadSpecialSettings(name);
+#ifdef _WIN32
     g_settings->res_data_org = g_settings->res_data;
+#endif
 
 #ifdef FPS
     fps_last.SetToNow();
@@ -1406,7 +1430,11 @@ int CALL InitiateGFX(GFX_INFO Gfx_Info)
     if (fb_depth_render_enabled)
         ZLUT_init();
 
+#ifdef _WIN32
     grConfigWrapperExt(g_settings->wrpResolution, g_settings->wrpVRAM * 1024 * 1024, g_settings->wrpFBO, g_settings->wrpAnisotropic);
+#else
+    grConfigWrapperExt(g_settings->wrpVRAM * 1024 * 1024, g_settings->wrpFBO, g_settings->wrpAnisotropic);
+#endif
 
     grGlideInit();
     grSstSelect(0);
@@ -1473,7 +1501,12 @@ void CALL PluginLoaded(void)
     general_setting(Set_texenh_options, "texenh_options", 0);
     general_setting(Set_hotkeys, "hotkeys", 1);
     general_setting(Set_wrpVRAM, "wrpVRAM", 0);
+#ifndef ANDROID
     general_setting(Set_wrpFBO, "wrpFBO", 0);
+#else
+    general_setting(Set_Rotate, "rotate", 0);
+    general_setting(Set_wrpFBO, "wrpFBO", 1);
+#endif
     general_setting(Set_wrpAnisotropic, "wrpAnisotropic", 0);
     general_setting(Set_autodetect_ucode, "autodetect_ucode", 1);
     general_setting(Set_ucode, "ucode", 2);
@@ -2322,6 +2355,33 @@ void CALL ViWidthChanged(void)
 {
 }
 
+#ifdef ANDROID
+/******************************************************************
+Function: SurfaceCreated
+Purpose:  this function is called when the surface is created.
+input:    none
+output:   none
+*******************************************************************/
+void CALL SurfaceCreated(void)
+{
+}
+
+/******************************************************************
+Function: SurfaceChanged
+Purpose:  this function is called when the surface is has changed.
+input:    none
+output:   none
+*******************************************************************/
+void init_combiner();
+
+void CALL SurfaceChanged(int width, int height)
+{
+    g_width = width;
+    g_height = height;
+    init_combiner();
+}
+#endif
+
 #ifdef WINPROC_OVERRIDE
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
@@ -2406,5 +2466,12 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode,
     }
 
     return CallNextHookEx(NULL, nCode, wParam, lParam);
+}
+#endif
+
+#ifdef ANDROID
+void Android_JNI_SwapWindow()
+{
+    gfx.SwapBuffers();
 }
 #endif

Source/Glide64/Settings.cpp
@@ -1,97 +1,104 @@
 #include "Gfx_1.3.h"
 
 CSettings::CSettings() :
-    card_id(0),
-    res_x(640), 
-	scr_res_x(640),
-    res_y(480),
-	scr_res_y(480),
-    res_data(GR_RESOLUTION_640x480), 
-	res_data_org(GR_RESOLUTION_640x480),
-    advanced_options(0),
-    texenh_options(0),
-    ssformat(0),
-    vsync(0),
-
-    show_fps(0),
-    clock(0),
-    clock_24_hr(0),
-
-    filtering(0),
-    fog(0),
-    buff_clear(0),
-    swapmode(0),
-    lodmode(0),
-    aspectmode(0),
-    use_hotkeys(0),
-#ifdef TEXTURE_FILTER
-    //Texture filtering options
-    texture_dir(""),
-    ghq_fltr(0),
-    ghq_enht(0),
-    ghq_cmpr(0),
-    ghq_hirs(0),
-    ghq_use(0),
-    ghq_enht_cmpr(0),
-    ghq_enht_tile(0),
-    ghq_enht_f16bpp(0),
-    ghq_enht_gz(0),
-    ghq_enht_nobg(0),
-    ghq_hirs_cmpr(0),
-    ghq_hirs_tile(0),
-    ghq_hirs_f16bpp(0),
-    ghq_hirs_gz(0),
-    ghq_hirs_altcrc(0),
-    ghq_cache_save(0),
-    ghq_cache_size(0),
-    ghq_hirs_let_texartists_fly(0),
-    ghq_hirs_dump(0),
+card_id(0),
+res_x(640),
+scr_res_x(640),
+res_y(480),
+scr_res_y(480),
+#ifdef _WIN32
+res_data(GR_RESOLUTION_640x480),
+res_data_org(GR_RESOLUTION_640x480),
 #endif
-    autodetect_ucode(0),
-    ucode(0),
-    logging(0),
-    elogging(0),
-    log_clear(0),
-    run_in_window(0),
-    filter_cache(0),
-    unk_as_red(0),
-    log_unk(0),
-    unk_clear(0),
-    wireframe(0),
-    wfmode(0),
+advanced_options(0),
+texenh_options(0),
+ssformat(0),
+vsync(0),
 
-    // Special fixes
-    offset_x(0), 
-	offset_y(0),
-    scale_x(0), 
-	scale_y(0),
-    fast_crc(0),
-    alt_tex_size(0),
-    use_sts1_only(0),
-    flame_corona(0), //hack for zeldas flame's corona
-    increase_texrect_edge(0), // add 1 to lower right corner coordinates of texrect
-    decrease_fillrect_edge(0), // sub 1 from lower right corner coordinates of fillrect
-    texture_correction(0), // enable perspective texture correction emulation. is on by default
-    stipple_mode(0), //used for dithered alpha emulation
-    stipple_pattern(0), //used for dithered alpha emulation
-    force_microcheck(0), //check microcode each frame, for mixed F3DEX-S2DEX games
-    force_quad3d(0), //force 0xb5 command to be quad, not line 3d
-    clip_zmin(0), //enable near z clipping
-    clip_zmax(0), //enable far plane clipping;
-    adjust_aspect(0), //adjust screen aspect for wide screen mode
-    force_calc_sphere(0), //use spheric mapping only, Ridge Racer 64
-    pal230(0),    //set special scale for PAL games
-    correct_viewport(0), //correct viewport values
-    zmode_compare_less(0), //force GR_CMP_LESS for zmode=0 (opaque)and zmode=1 (interpenetrating)
-    old_style_adither(0), //apply alpha dither regardless of alpha_dither_mode
-    n64_z_scale(0), //scale vertex z value before writing to depth buffer, as N64 does.
+show_fps(0),
+clock(0),
+clock_24_hr(0),
+#ifdef ANDROID
+rotate(0),
+#endif
 
-	hacks(0),
+filtering(0),
+fog(0),
+buff_clear(0),
+swapmode(0),
+lodmode(0),
+aspectmode(0),
+use_hotkeys(0),
+#ifdef TEXTURE_FILTER
+//Texture filtering options
+texture_dir(""),
+ghq_fltr(0),
+ghq_enht(0),
+ghq_cmpr(0),
+ghq_hirs(0),
+ghq_use(0),
+ghq_enht_cmpr(0),
+ghq_enht_tile(0),
+ghq_enht_f16bpp(0),
+ghq_enht_gz(0),
+ghq_enht_nobg(0),
+ghq_hirs_cmpr(0),
+ghq_hirs_tile(0),
+ghq_hirs_f16bpp(0),
+ghq_hirs_gz(0),
+ghq_hirs_altcrc(0),
+ghq_cache_save(0),
+ghq_cache_size(0),
+ghq_hirs_let_texartists_fly(0),
+ghq_hirs_dump(0),
+#endif
+autodetect_ucode(0),
+ucode(0),
+logging(0),
+elogging(0),
+log_clear(0),
+run_in_window(0),
+filter_cache(0),
+unk_as_red(0),
+log_unk(0),
+unk_clear(0),
+wireframe(0),
+wfmode(0),
 
-    //wrapper settings
-    wrpResolution(0),
-    wrpVRAM(0),
-    wrpFBO(0),
-    wrpAnisotropic(0)
+// Special fixes
+offset_x(0),
+offset_y(0),
+scale_x(0),
+scale_y(0),
+fast_crc(0),
+alt_tex_size(0),
+use_sts1_only(0),
+flame_corona(0), //hack for zeldas flame's corona
+increase_texrect_edge(0), // add 1 to lower right corner coordinates of texrect
+decrease_fillrect_edge(0), // sub 1 from lower right corner coordinates of fillrect
+texture_correction(0), // enable perspective texture correction emulation. is on by default
+stipple_mode(0), //used for dithered alpha emulation
+stipple_pattern(0), //used for dithered alpha emulation
+force_microcheck(0), //check microcode each frame, for mixed F3DEX-S2DEX games
+force_quad3d(0), //force 0xb5 command to be quad, not line 3d
+clip_zmin(0), //enable near z clipping
+clip_zmax(0), //enable far plane clipping;
+adjust_aspect(0), //adjust screen aspect for wide screen mode
+force_calc_sphere(0), //use spheric mapping only, Ridge Racer 64
+pal230(0),    //set special scale for PAL games
+correct_viewport(0), //correct viewport values
+zmode_compare_less(0), //force GR_CMP_LESS for zmode=0 (opaque)and zmode=1 (interpenetrating)
+old_style_adither(0), //apply alpha dither regardless of alpha_dither_mode
+n64_z_scale(0), //scale vertex z value before writing to depth buffer, as N64 does.
+
+hacks(0),
+
+//wrapper settings
+#ifdef _WIN32
+wrpResolution(0),
+#endif
+wrpVRAM(0),
+wrpFBO(0),
+wrpAnisotropic(0)
 {
 }

Source/Glide64/Settings.h
@@ -9,7 +9,9 @@ public:
 
     uint32_t res_x, scr_res_x;
     uint32_t res_y, scr_res_y;
+#ifdef _WIN32
     uint32_t res_data, res_data_org;
+#endif
 
     int advanced_options;
     int texenh_options;
@@ -19,6 +21,9 @@ public:
     int show_fps;
     int clock;
     int clock_24_hr;
+#ifdef ANDROID
+    int rotate;
+#endif
 
     int filtering;
     int fog;
@@ -152,7 +157,9 @@ public:
     uint32_t hacks;
 
     //wrapper settings
+#ifdef _WIN32
     int wrpResolution;
+#endif
     int wrpVRAM;
     int wrpFBO;
     int wrpAnisotropic;

Source/Glitch64/glitchmain.h
@@ -17,7 +17,9 @@ extern int dumping;
 
 typedef struct _wrapper_config
 {
+#ifdef _WIN32
     int res;
+#endif
     int fbo;
     int anisofilter;
     int vram_size;
@@ -117,6 +119,17 @@ extern PFNGLGETINFOLOGARBPROC glGetInfoLogARB;
 extern PFNGLGETOBJECTPARAMETERIVARBPROC glGetObjectParameterivARB;
 extern PFNGLSECONDARYCOLOR3FPROC glSecondaryColor3f;
 #endif
+void check_compile(GLuint shader);
+void check_link(GLuint program);
+void vbo_enable();
+void vbo_disable();
+
+//Vertex Attribute Locations
+#define POSITION_ATTR 0
+#define COLOUR_ATTR 1
+#define TEXCOORD_0_ATTR 2
+#define TEXCOORD_1_ATTR 3
+#define FOG_ATTR 4
 
 extern int w_buffer_mode;
 extern int nbTextureUnits;
@@ -154,11 +167,15 @@ void free_combiners();
 void compile_shader();
 void set_lambda();
 void set_copy_shader();
+void disable_textureSizes();
 
 // config functions
 
-//FX_ENTRY void FX_CALL grConfigWrapperExt(HINSTANCE instance, HWND hwnd);
+#ifdef _WIN32
 FX_ENTRY void FX_CALL grConfigWrapperExt(FxI32, FxI32, FxBool, FxBool);
+#else
+FX_ENTRY void FX_CALL grConfigWrapperExt(FxI32, FxBool, FxBool);
+#endif
 FX_ENTRY GrScreenResolution_t FX_CALL grWrapperFullScreenResolutionExt(FxU32*, FxU32*);
 FX_ENTRY char ** FX_CALL grQueryResolutionsExt(int32_t*);
 FX_ENTRY FxBool FX_CALL grKeyPressedExt(FxU32 key);

Source/Glitch64/inc/glide.h
@@ -623,15 +623,11 @@ grFinish(void);
 FX_ENTRY void FX_CALL 
 grFlush(void);
 
-FX_ENTRY GrContext_t FX_CALL 
-grSstWinOpen(
-          HWND                 hWnd,
-          GrScreenResolution_t screen_resolution,
-          GrScreenRefresh_t    refresh_rate,
-          GrColorFormat_t      color_format,
-          GrOriginLocation_t   origin_location,
-          int                  nColBuffers,
-          int                  nAuxBuffers);
+#ifdef _WIN32
+FX_ENTRY GrContext_t FX_CALL grSstWinOpen( HWND hWnd, GrScreenResolution_t screen_resolution, GrScreenRefresh_t refresh_rate, GrColorFormat_t color_format, GrOriginLocation_t origin_location, int  nColBuffers, int nAuxBuffers);
+#else
+FX_ENTRY GrContext_t FX_CALL grSstWinOpen( GrScreenRefresh_t refresh_rate, GrColorFormat_t color_format, GrOriginLocation_t origin_location, int  nColBuffers, int nAuxBuffers);
+#endif
 
 FX_ENTRY FxBool FX_CALL
 grSstWinClose( GrContext_t context );

Source/Glide64/Gfx_1.3.h
@@ -261,6 +261,9 @@ extern "C" {
         uint32_t * VI_Y_SCALE_REG;
 
         void(*CheckInterrupts)(void);
+#ifdef ANDROID
+        void(CALL *SwapBuffers)(void);
+#endif
     } GFX_INFO;
 
     extern GFX_INFO gfx;
@@ -490,6 +493,22 @@ extern "C" {
     *******************************************************************/
     EXPORT void CALL ViWidthChanged(void);
 
+#ifdef ANDROID
+    /******************************************************************
+    Function: SurfaceCreated
+    Purpose:  this function is called when the surface is created.
+    input:    none
+    output:   none
+    *******************************************************************/
+    EXPORT void CALL SurfaceCreated(void);
+    /******************************************************************
+    Function: SurfaceChanged
+    Purpose:  this function is called when the surface is has changed.
+    input:    none
+    output:   none
+    *******************************************************************/
+    EXPORT void CALL SurfaceChanged(int width, int height);
+#endif
     /******************************************************************
     Function: FrameBufferWrite
     Purpose:  This function is called to notify the dll that the

Source/Glide64/trace.cpp
@@ -44,15 +44,13 @@ void SetupTrace(void)
         g_AndroidLogger = new AndroidLogger();
     }
     TraceAddModule(g_AndroidLogger);
-
-    TraceSetMaxModule(MaxTraceModuleGlide64, TraceVerbose);
-#else
+#endif
 #ifdef _DEBUG
     TraceSetMaxModule(MaxTraceModuleGlide64, TraceInfo);
 #else
     TraceSetMaxModule(MaxTraceModuleGlide64, TraceError);
 #endif
-#endif
+
 
     TraceSetModuleName(TraceMD5, "MD5");
     TraceSetModuleName(TraceSettings, "Settings");
@@ -64,6 +62,7 @@ void SetupTrace(void)
     TraceSetModuleName(TraceRDP, "RDP");
     TraceSetModuleName(TraceTLUT, "TLUT");
     TraceSetModuleName(TracePNG, "PNG");
+    TraceSetModuleName(TraceOGLWrapper, "OGL Wrapper");
 
     char log_dir[260];
     memset(log_dir, 0, sizeof(log_dir));

commit 57f082f1852be1e0c3d8b949a7aa45a0ae5e86c4

[Glide64] [Glitch64] fixed non-WIN32 regressions from 52aea87

Source/Glide64/GlideExtensions.h
@@ -44,20 +44,31 @@ typedef FxU32 GrCombineMode_t;
 
 #define GR_TEXTURE_UMA_EXT                0x06
 //wrapper specific
-#ifdef _WIN32
-FX_ENTRY void FX_CALL grConfigWrapperExt(FxI32, FxI32, FxBool, FxBool);
-#else
-FX_ENTRY void FX_CALL grConfigWrapperExt(FxI32, FxBool, FxBool);
+FX_ENTRY void FX_CALL grConfigWrapperExt(
+#ifndef ANDROID
+    FxI32, /* resolution parameter not supported on Android */
 #endif
+    FxI32,
+    FxBool,
+    FxBool
+);
 FX_ENTRY GrScreenResolution_t FX_CALL grWrapperFullScreenResolutionExt(FxU32*, FxU32*);
 FX_ENTRY char ** FX_CALL grQueryResolutionsExt(int32_t*);
 FX_ENTRY FxBool FX_CALL grKeyPressedExt(FxU32 key);
 FX_ENTRY void FX_CALL grGetGammaTableExt(FxU32, FxU32*, FxU32*, FxU32*);
 
-#ifdef _WIN32
-FX_ENTRY GrContext_t FX_CALL grSstWinOpenExt(HWND hWnd, GrScreenResolution_t screen_resolution, GrScreenRefresh_t refresh_rate, GrColorFormat_t color_format, GrOriginLocation_t origin_location, GrPixelFormat_t pixelformat, int nColBuffers, int nAuxBuffers);
-#else
-FX_ENTRY GrContext_t FX_CALL grSstWinOpenExt(GrScreenRefresh_t refresh_rate, GrColorFormat_t color_format, GrOriginLocation_t origin_location, GrPixelFormat_t pixelformat, int nColBuffers, int nAuxBuffers);
+FX_ENTRY GrContext_t FX_CALL grSstWinOpenExt(
+#ifndef ANDROID
+    HWND hWnd,
+    GrScreenResolution_t screen_resolution,
+#endif
+    GrScreenRefresh_t refresh_rate,
+    GrColorFormat_t color_format,
+    GrOriginLocation_t origin_location,
+    GrPixelFormat_t pixelformat,
+    int nColBuffers,
+    int nAuxBuffers
+);
 #endif
 
 //color combiner

Source/Glide64/Main.cpp
@@ -281,11 +281,13 @@ void ChangeSize()
 
 void ConfigWrapper()
 {
-#ifdef _WIN32
-    grConfigWrapperExt(g_settings->wrpResolution, g_settings->wrpVRAM * 1024 * 1024, g_settings->wrpFBO, g_settings->wrpAnisotropic);
+    grConfigWrapperExt(
+#ifdef ANDROID
+        g_settings->wrpVRAM * 1024 * 1024, g_settings->wrpFBO, g_settings->wrpAnisotropic
 #else
-    grConfigWrapperExt(g_settings->wrpVRAM * 1024 * 1024, g_settings->wrpFBO, g_settings->wrpAnisotropic);
+        g_settings->wrpResolution, g_settings->wrpVRAM * 1024 * 1024, g_settings->wrpFBO, g_settings->wrpAnisotropic
 #endif
+    );
 }
 
 void UseUnregisteredSetting(int /*SettingID*/)
@@ -859,6 +861,10 @@ int InitGfx()
     }
     //*/
 
+/*
+ * 2016.03.27 cxd4 -- to do:  #ifdef _WIN32 or #ifndef _ANDROID?
+ * Can't tell which one is correct here, but I think the latter.
+ */
 #ifdef _WIN32
     uint32_t res_data = g_settings->res_data;
     if (ev_fullscreen)
@@ -1401,7 +1407,7 @@ int CALL InitiateGFX(GFX_INFO Gfx_Info)
     ReadSettings();
     char name[21] = "DEFAULT";
     ReadSpecialSettings(name);
-#ifdef _WIN32
+#ifndef ANDROID
     g_settings->res_data_org = g_settings->res_data;
 #endif
 
@@ -1430,11 +1436,13 @@ int CALL InitiateGFX(GFX_INFO Gfx_Info)
     if (fb_depth_render_enabled)
         ZLUT_init();
 
+    grConfigWrapperExt(
 #ifdef _WIN32
-    grConfigWrapperExt(g_settings->wrpResolution, g_settings->wrpVRAM * 1024 * 1024, g_settings->wrpFBO, g_settings->wrpAnisotropic);
+        g_settings->wrpResolution, g_settings->wrpVRAM * 1024 * 1024, g_settings->wrpFBO, g_settings->wrpAnisotropic
 #else
-    grConfigWrapperExt(g_settings->wrpVRAM * 1024 * 1024, g_settings->wrpFBO, g_settings->wrpAnisotropic);
+        g_settings->wrpVRAM * 1024 * 1024, g_settings->wrpFBO, g_settings->wrpAnisotropic
 #endif
+    );
 
     grGlideInit();
     grSstSelect(0);

Source/Glide64/Settings.cpp
@@ -6,7 +6,7 @@ res_x(640),
 scr_res_x(640),
 res_y(480),
 scr_res_y(480),
-#ifdef _WIN32
+#ifndef ANDROID
 res_data(GR_RESOLUTION_640x480),
 res_data_org(GR_RESOLUTION_640x480),
 #endif
@@ -94,7 +94,7 @@ n64_z_scale(0), //scale vertex z value before writing to depth buffer, as N64 do
 hacks(0),
 
 //wrapper settings
-#ifdef _WIN32
+#ifndef ANDROID
 wrpResolution(0),
 #endif
 wrpVRAM(0),

Source/Glide64/Settings.h
@@ -9,7 +9,7 @@ public:
 
     uint32_t res_x, scr_res_x;
     uint32_t res_y, scr_res_y;
-#ifdef _WIN32
+#ifndef ANDROID
     uint32_t res_data, res_data_org;
 #endif
 
@@ -157,7 +157,7 @@ public:
     uint32_t hacks;
 
     //wrapper settings
-#ifdef _WIN32
+#ifndef ANDROID
     int wrpResolution;
 #endif
     int wrpVRAM;

Source/Glitch64/glitchmain.h
@@ -17,7 +17,7 @@ extern int dumping;
 
 typedef struct _wrapper_config
 {
-#ifdef _WIN32
+#ifndef ANDROID
     int res;
 #endif
     int fbo;
@@ -178,11 +178,14 @@ void disable_textureSizes();
 
 // config functions
 
-#ifdef _WIN32
-FX_ENTRY void FX_CALL grConfigWrapperExt(FxI32, FxI32, FxBool, FxBool);
-#else
-FX_ENTRY void FX_CALL grConfigWrapperExt(FxI32, FxBool, FxBool);
+FX_ENTRY void FX_CALL grConfigWrapperExt(
+#ifndef ANDROID
+    FxI32, /* resolution parameter not supported on Android build */
 #endif
+    FxI32,
+    FxBool,
+    FxBool
+);
 FX_ENTRY GrScreenResolution_t FX_CALL grWrapperFullScreenResolutionExt(FxU32*, FxU32*);
 FX_ENTRY char ** FX_CALL grQueryResolutionsExt(int32_t*);
 FX_ENTRY FxBool FX_CALL grKeyPressedExt(FxU32 key);

Source/Glitch64/inc/glide.h
@@ -623,11 +623,13 @@ grFinish(void);
 FX_ENTRY void FX_CALL 
 grFlush(void);
 
-#ifdef _WIN32
-FX_ENTRY GrContext_t FX_CALL grSstWinOpen( HWND hWnd, GrScreenResolution_t screen_resolution, GrScreenRefresh_t refresh_rate, GrColorFormat_t color_format, GrOriginLocation_t origin_location, int  nColBuffers, int nAuxBuffers);
+FX_ENTRY GrContext_t FX_CALL grSstWinOpen(
+#ifdef ANDROID
+    GrScreenRefresh_t refresh_rate, GrColorFormat_t color_format, GrOriginLocation_t origin_location, int  nColBuffers, int nAuxBuffers
 #else
-FX_ENTRY GrContext_t FX_CALL grSstWinOpen( GrScreenRefresh_t refresh_rate, GrColorFormat_t color_format, GrOriginLocation_t origin_location, int  nColBuffers, int nAuxBuffers);
+    HWND hWnd, GrScreenResolution_t screen_resolution, GrScreenRefresh_t refresh_rate, GrColorFormat_t color_format, GrOriginLocation_t origin_location, int  nColBuffers, int nAuxBuffers
 #endif
+);
 
 FX_ENTRY FxBool FX_CALL
 grSstWinClose( GrContext_t context );