78184ad32ac6afd0e079a35e05effbaef3186ac6
zilmar <zilmar@pj64-emu.com>
2016-06-28 04:13:35 +1000

    [Project64] Code clean up of ExitInfo.h

--- Source/Project64-core/N64System/Recompiler/ExitInfo.h
+++ Source/Project64-core/N64System/Recompiler/ExitInfo.h
@@ -14,25 +14,25 @@
 
 struct CExitInfo
 {
-	enum EXIT_REASON
-	{
-		Normal = 0,
-		Normal_NoSysCheck = 1,
-		DoCPU_Action = 2,
-		COP1_Unuseable = 3,
-		DoSysCall = 4,
-		TLBReadMiss = 5,
-		TLBWriteMiss = 6,
-		DivByZero = 7,
-		ExitResetRecompCode = 8,
-	};
+    enum EXIT_REASON
+    {
+        Normal = 0,
+        Normal_NoSysCheck = 1,
+        DoCPU_Action = 2,
+        COP1_Unuseable = 3,
+        DoSysCall = 4,
+        TLBReadMiss = 5,
+        TLBWriteMiss = 6,
+        DivByZero = 7,
+        ExitResetRecompCode = 8,
+    };
 
-	uint32_t    ID;
-	uint32_t    TargetPC;
-	CRegInfo    ExitRegSet;
-	EXIT_REASON reason;
-	STEP_TYPE   NextInstruction;
-	uint32_t *  JumpLoc; //32bit jump
+    uint32_t    ID;
+    uint32_t    TargetPC;
+    CRegInfo    ExitRegSet;
+    EXIT_REASON reason;
+    STEP_TYPE   NextInstruction;
+    uint32_t *  JumpLoc; //32bit jump
 };
 
 typedef std::list<CExitInfo> EXIT_LIST;
