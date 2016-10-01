7f83a526416172591d3bcfd45d3eb3d68ad2cb05
zilmar <zilmar@pj64-emu.com>
2016-10-02 09:58:42 +1100

    [Android] default to basic settings

--- Android/src/emu/project64/settings/SettingsFragment.java
+++ Android/src/emu/project64/settings/SettingsFragment.java
@@ -13,22 +13,16 @@ package emu.project64.settings;
 import emu.project64.R;
 import emu.project64.jni.NativeExports;
 import emu.project64.jni.SettingsID;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.support.v7.preference.ListPreference;
-import android.support.v7.preference.Preference;
-import android.support.v7.preference.PreferenceManager;
 
 public class SettingsFragment extends BaseSettingsFragment
 {
     @Override
     protected int getXml() 
     {
-    	if (!NativeExports.SettingsLoadBool(SettingsID.UserInterface_BasicMode.getValue()))
-    	{
-            return R.xml.settings_advanced;
-    	}
+        if (NativeExports.SettingsLoadBool(SettingsID.UserInterface_BasicMode.getValue()))
+        {
+            return R.xml.settings_basic;
+        }
         return R.xml.settings;
     }
 
@@ -36,19 +30,5 @@ public class SettingsFragment extends BaseSettingsFragment
     protected int getTitleId() 
     {
         return R.string.preferences;
-    }
-
-    @Override
-    public boolean onPreferenceTreeClick(Preference preference)
-    {
-    	if (preference.getKey().equals("logging_core"))
-    	{
-            loadFragment(new LoggingProject64Core());
-    	}
-    	else
-    	{
-            return super.onPreferenceTreeClick(preference);
-    	}
-        return true;
     }
 }
