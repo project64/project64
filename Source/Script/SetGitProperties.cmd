a65968171e70fa02aea07fe1945df952dce9d995
zilmar <zilmar@pj64-emu.com>
2017-04-08 09:36:32 +1000

    Update to latest build scripts

---
+++ Source/Script/SetGitProperties.cmd
@@ -0,0 +1,28 @@
+@echo off
+SETLOCAL
+
+set origdir=%cd%
+cd /d %~dp0..\..
+set base_dir=%cd%
+cd /d %origdir%
+
+for /f %%i in ('git describe --tags --long') do set GIT_DESCRIBE=%%i > nul
+
+for /F "tokens=1,2,3 delims=-" %%i in ("%GIT_DESCRIBE%") do call :process_git_desc %%i %%j %%k
+for /F "tokens=1,2,3" %%i in (%base_dir%\Source\Project64-core\version.h) do call :process_version %%i %%j %%k
+set VERSION=v%VERSION_MAJOR%.%VERSION_MINOR%.%VERSION_REVISION%-%VERSION_BUILD%-%VERSION_COMMIT%
+
+echo GIT_DESCRIBE = %VERSION% > "%base_dir%\git.properties"
+goto :EOF
+
+
+:process_git_desc
+set VERSION_BUILD=%2
+set VERSION_COMMIT=%3
+goto :EOF
+
+:process_version
+if "%1" == "#define" if "%2" == "VERSION_MAJOR" set VERSION_MAJOR=%3
+if "%1" == "#define" if "%2" == "VERSION_MINOR" set VERSION_MINOR=%3
+if "%1" == "#define" if "%2" == "VERSION_REVISION" set VERSION_REVISION=%3
+goto :EOF
