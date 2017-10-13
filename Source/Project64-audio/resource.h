125cbc79cf25aa79970bf5d097cf279ebc9bff54
Frank-74 <franktriggs@talktalk.net>
2017-10-13 03:21:14 +0100

    Update resource.h
    fix broken dialog

--- Source/Project64-audio/resource.h
+++ Source/Project64-audio/resource.h
@@ -1,14 +1,21 @@
-﻿//{{NO_DEPENDENCIES}}
+//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
 // Used by Project64-audio.rc
 //
+#define VERSION_MINOR                   0
+#define VER_VER_DEBUG                   0
+#define VERSION_MAJOR                   1
+#define VERSION_REVISION                1
 #define IDD_CONFIG                      9
 #define IDD_LOG_SETTINGS                103
 #define IDD_GAME_SETTING                104
-#define IDC_TINYBUFFER                  1001
-#define IDC_FPSBUFFER                   1002
+#define IDC_BUFFER_DIVIDER              1001
+#define IDC_BUFFER_LEVEL                1002
 #define IDC_VOLUME                      1003
 #define IDC_MUTE                        1004
+#define IDC_FPSBUFFER                   1004
+#define IDC_CHECK2                      1005
+#define IDC_TINYBUFFER                  1005
 #define IDC_CMB_TRACE_PATH              1019
 #define IDC_CMB_TRACE_INIT_SHUTDOWN     1020
 #define IDC_CMB_TRACE_INTERFACE         1021
@@ -19,6 +26,7 @@
 #define IDC_TXT_TRACE_INTERFACE         1082
 #define IDC_TXT_TRACE_AUDIO_DRIVER      1083
 #define IDC_CMB_TRACE_THREAD            1090
+#define VERSION_BUILD                   9999
 
 // Next default values for new objects
 // 
@@ -26,7 +34,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        105
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1006
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
