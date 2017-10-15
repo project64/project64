f57e2f6bf775f43d3c6f0fd825547be971065bb7
Frank-74 <franktriggs@talktalk.net>
2017-10-15 17:50:07 +0100

    Fix tabs/spaces

--- Source/Project64-audio/Driver/DirectSound.cpp
+++ Source/Project64-audio/Driver/DirectSound.cpp
@@ -132,16 +132,16 @@ void DirectSoundDriver::SetFrequency(uint32_t Frequency, uint32_t BufferSize)
 
 void DirectSoundDriver::SetVolume(uint32_t Volume)
 {
-	LPDIRECTSOUNDBUFFER & lpdsb = (LPDIRECTSOUNDBUFFER &)m_lpdsb;
-	int32_t dsVolume = -((100 - (int32_t)Volume) * 25);
-	if (Volume == 0)
-	{
-		dsVolume = DSBVOLUME_MIN;
-	}
-	if (lpdsb != NULL)
-	{
-		lpdsb->SetVolume(dsVolume);
-	}
+    LPDIRECTSOUNDBUFFER & lpdsb = (LPDIRECTSOUNDBUFFER &)m_lpdsb;
+    int32_t dsVolume = -((100 - (int32_t)Volume) * 25);
+    if (Volume == 0)
+    {
+        dsVolume = DSBVOLUME_MIN;
+    }
+    if (lpdsb != NULL)
+    {
+        lpdsb->SetVolume(dsVolume);
+    }
 }
 
 void DirectSoundDriver::SetSegmentSize(uint32_t length, uint32_t SampleRate)
