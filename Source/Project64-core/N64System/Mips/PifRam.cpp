35df55831047829de40c9b91a66c2dd2c3590257
Azimer <Azimer@users.noreply.github.com>
2017-10-17 16:54:25 -0500

    Formatting take 2

--- Source/Project64-core/N64System/Mips/PifRam.cpp
+++ Source/Project64-core/N64System/Mips/PifRam.cpp
@@ -362,10 +362,10 @@ void CPifRam::SI_DMA_READ()
     }
     else
     {
-		g_SystemTimer->SetTimer(CSystemTimer::SiTimer, rand() % 0x40, false);
-		//g_Reg->MI_INTR_REG |= MI_INTR_SI;
-		//g_Reg->SI_STATUS_REG |= SI_STATUS_INTERRUPT;
-		//g_Reg->CheckInterrupts();
+        g_SystemTimer->SetTimer(CSystemTimer::SiTimer, rand() % 0x40, false);
+        //g_Reg->MI_INTR_REG |= MI_INTR_SI;
+        //g_Reg->SI_STATUS_REG |= SI_STATUS_INTERRUPT;
+        //g_Reg->CheckInterrupts();
     }
 }
 
