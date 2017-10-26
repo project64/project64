a3ba186f3960fc8f097e21c27f088e76cda8e1cd
Julio C. Rocha <julio@rochsquadron.net>
2017-10-25 22:16:35 -0700

    Fixed SolutionDir location.

--- Source/Script/UpdateVersion.ps1
+++ Source/Script/UpdateVersion.ps1
@@ -1,22 +1,22 @@
 Param
 (
-	$SolutionDir = $(Get-Location),
+	$SolutionDir = (Split-path (Split-Path ($PSScriptRoot))),
 	[Parameter(Mandatory=$true)]
 	[string] $Version,
 	[string[]] $Files = (
-		'Source\Project64-core\version.h',
-		'Source\nragev20\version.h',
-		'Source\RSP\version.h',
-		'Source\Project64-video\version.h'
+		"$SolutionDir\Source\Project64-core\version.h",
+		"$SolutionDir\Source\nragev20\version.h",
+		"$SolutionDir\Source\RSP\version.h",
+		"$SolutionDir\Source\Project64-video\version.h"
 	)
 )
 
 $versionArr = $Version.Split('.')
 foreach ($file in $Files) {
-	(Get-Content "$SolutionDir\$file") `
+	(Get-Content $file) `
 		-replace '(#define\s+VERSION_MAJOR\s+)\d+',    "`${1}$($versionArr[0])" `
 		-replace '(#define\s+VERSION_MINOR\s+)\d+',    "`${1}$($versionArr[1])" `
 		-replace '(#define\s+VERSION_REVISION\s+)\d+', "`${1}$($versionArr[2])" `
 		-replace '(#define\s+VERSION_BUILD\s+)\d+',    "`${1}$($versionArr[3])" `
-	| Set-Content "$SolutionDir\$file"
+	| Set-Content $file
 }
