bab9d93ebbee1f9991a3fe858c0e8e850c4e4c1b
zilmar <zilmar@pj64-emu.com>
2018-04-09 18:03:38 +1000

    [Project64] Make sure CSettingTypeApplicationIndex use uint32_t

--- Source/Project64-core/Settings/SettingType/SettingsType-ApplicationIndex.cpp
+++ Source/Project64-core/Settings/SettingType/SettingsType-ApplicationIndex.cpp
@@ -12,94 +12,94 @@
 #include "SettingsType-Application.h"
 #include "SettingsType-ApplicationIndex.h"
 
-CSettingTypeApplicationIndex::CSettingTypeApplicationIndex(const char * Section, const char * Name, uint32_t DefaultValue ) :
-    CSettingTypeApplication(Section,Name,DefaultValue)
+CSettingTypeApplicationIndex::CSettingTypeApplicationIndex(const char * Section, const char * Name, uint32_t DefaultValue) :
+    CSettingTypeApplication(Section, Name, DefaultValue)
 {
 }
 
-CSettingTypeApplicationIndex::CSettingTypeApplicationIndex(const char * Section, const char * Name, bool DefaultValue ) :
-    CSettingTypeApplication(Section,Name,DefaultValue)
+CSettingTypeApplicationIndex::CSettingTypeApplicationIndex(const char * Section, const char * Name, bool DefaultValue) :
+    CSettingTypeApplication(Section, Name, DefaultValue)
 {
 }
 
-CSettingTypeApplicationIndex::CSettingTypeApplicationIndex(const char * Section, const char * Name, const char * DefaultValue ) :
-    CSettingTypeApplication(Section,Name,DefaultValue)
+CSettingTypeApplicationIndex::CSettingTypeApplicationIndex(const char * Section, const char * Name, const char * DefaultValue) :
+    CSettingTypeApplication(Section, Name, DefaultValue)
 {
 }
 
-CSettingTypeApplicationIndex::CSettingTypeApplicationIndex(const char * Section, const char * Name, SettingID DefaultSetting ) :
-    CSettingTypeApplication(Section,Name,DefaultSetting)
+CSettingTypeApplicationIndex::CSettingTypeApplicationIndex(const char * Section, const char * Name, SettingID DefaultSetting) :
+    CSettingTypeApplication(Section, Name, DefaultSetting)
 {
 }
 
-CSettingTypeApplicationIndex::~CSettingTypeApplicationIndex ( void )
+CSettingTypeApplicationIndex::~CSettingTypeApplicationIndex(void)
 {
 }
 
-bool CSettingTypeApplicationIndex::Load ( int Index, bool & Value ) const
+bool CSettingTypeApplicationIndex::Load(uint32_t Index, bool & Value) const
 {
-    m_KeyNameIdex = stdstr_f("%s %d",m_KeyName.c_str(),Index);
-    return CSettingTypeApplication::Load(0,Value);
+    m_KeyNameIdex = stdstr_f("%s %d", m_KeyName.c_str(), Index);
+    return CSettingTypeApplication::Load(0, Value);
 }
 
-bool CSettingTypeApplicationIndex::Load ( int Index, uint32_t & Value ) const
+bool CSettingTypeApplicationIndex::Load(uint32_t Index, uint32_t & Value) const
 {
-    m_KeyNameIdex = stdstr_f("%s %d",m_KeyName.c_str(),Index);
-    return CSettingTypeApplication::Load(0,Value);
+    m_KeyNameIdex = stdstr_f("%s %d", m_KeyName.c_str(), Index);
+    return CSettingTypeApplication::Load(0, Value);
 }
 
-bool CSettingTypeApplicationIndex::Load ( int Index, stdstr & Value ) const
+bool CSettingTypeApplicationIndex::Load(uint32_t Index, std::string & Value) const
 {
-    m_KeyNameIdex = stdstr_f("%s %d",m_KeyName.c_str(),Index);
-    return CSettingTypeApplication::Load(0,Value);
+    m_KeyNameIdex = stdstr_f("%s %d", m_KeyName.c_str(), Index);
+    return CSettingTypeApplication::Load(0, Value);
 }
 
 //return the default values
-void CSettingTypeApplicationIndex::LoadDefault ( int Index, bool & Value   ) const
+void CSettingTypeApplicationIndex::LoadDefault(uint32_t Index, bool & Value) const
 {
-    m_KeyNameIdex = stdstr_f("%s %d",m_KeyName.c_str(),Index);
-    CSettingTypeApplication::LoadDefault(0,Value);
+    m_KeyNameIdex = stdstr_f("%s %d", m_KeyName.c_str(), Index);
+    CSettingTypeApplication::LoadDefault(0, Value);
 }
 
-void CSettingTypeApplicationIndex::LoadDefault ( int Index, uint32_t & Value  ) const
+void CSettingTypeApplicationIndex::LoadDefault(uint32_t Index, uint32_t & Value) const
 {
-    m_KeyNameIdex = stdstr_f("%s %d",m_KeyName.c_str(),Index);
-    CSettingTypeApplication::LoadDefault(0,Value);
+    m_KeyNameIdex = stdstr_f("%s %d", m_KeyName.c_str(), Index);
+    CSettingTypeApplication::LoadDefault(0, Value);
 }
 
-void CSettingTypeApplicationIndex::LoadDefault ( int Index, stdstr & Value ) const
+void CSettingTypeApplicationIndex::LoadDefault(uint32_t Index, std::string & Value) const
 {
-    m_KeyNameIdex = stdstr_f("%s %d",m_KeyName.c_str(),Index);
-    CSettingTypeApplication::LoadDefault(0,Value);
+    m_KeyNameIdex = stdstr_f("%s %d", m_KeyName.c_str(), Index);
+    CSettingTypeApplication::LoadDefault(0, Value);
 }
 
 //Update the settings
-void CSettingTypeApplicationIndex::Save ( int Index, bool Value )
+void CSettingTypeApplicationIndex::Save(uint32_t Index, bool Value)
 {
-    m_KeyNameIdex = stdstr_f("%s %d",m_KeyName.c_str(),Index);
-    CSettingTypeApplication::Save(0,Value);
+    m_KeyNameIdex = stdstr_f("%s %d", m_KeyName.c_str(), Index);
+    CSettingTypeApplication::Save(0, Value);
 }
 
-void CSettingTypeApplicationIndex::Save ( int Index, uint32_t Value )
+void CSettingTypeApplicationIndex::Save(uint32_t Index, uint32_t Value)
 {
-    m_KeyNameIdex = stdstr_f("%s %d",m_KeyName.c_str(),Index);
-    CSettingTypeApplication::Save(0,Value);
+    m_KeyNameIdex = stdstr_f("%s %d", m_KeyName.c_str(), Index);
+    CSettingTypeApplication::Save(0, Value);
 }
 
-void CSettingTypeApplicationIndex::Save ( int Index, const stdstr & Value )
+void CSettingTypeApplicationIndex::Save(uint32_t Index, const std::string & Value)
 {
-    m_KeyNameIdex = stdstr_f("%s %d",m_KeyName.c_str(),Index);
-    CSettingTypeApplication::Save(0,Value);
+    m_KeyNameIdex = stdstr_f("%s %d", m_KeyName.c_str(), Index);
+    CSettingTypeApplication::Save(0, Value);
 }
 
-void CSettingTypeApplicationIndex::Save ( int Index, const char * Value )
+void CSettingTypeApplicationIndex::Save(uint32_t Index, const char * Value)
 {
-    m_KeyNameIdex = stdstr_f("%s %d",m_KeyName.c_str(),Index);
-    CSettingTypeApplication::Save(0,Value);
+    m_KeyNameIdex = stdstr_f("%s %d", m_KeyName.c_str(), Index);
+    CSettingTypeApplication::Save(0, Value);
 }
 
-void CSettingTypeApplicationIndex::Delete ( int Index )
+void CSettingTypeApplicationIndex::Delete(uint32_t Index)
 {
-    m_KeyNameIdex = stdstr_f("%s %d",m_KeyName.c_str(),Index);
-    CSettingTypeApplication::Save(0,(const char *)NULL);
+    m_KeyNameIdex = stdstr_f("%s %d", m_KeyName.c_str(), Index);
+    CSettingTypeApplication::Save(0, (const char *)NULL);
 }
