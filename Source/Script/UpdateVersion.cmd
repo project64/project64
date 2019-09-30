ac0a9258bd7be9d0a69a662f0908599073ab3ea5
oddMLan <oddMLan2@gmail.com>
2019-09-29 19:17:28 -0700

    Pass Platform solution parameter to UpdateVersion.cmd and package_zip.cmd This adds the ability to package x64 builds if requested.

--- Source/Script/UpdateVersion.cmd
+++ Source/Script/UpdateVersion.cmd
@@ -6,17 +6,19 @@ cd /d %~dp0..\..
 set base_dir=%cd%
 cd /d %origdir%
 
-if "%~3"=="" (
-    echo Usage: UpdateVersion.cmd [BuildMode] [InFile] [OutFile]
+if "%~4"=="" (
+    echo Usage: UpdateVersion.cmd [BuildMode] [Platform] [InFile] [OutFile]
     goto :eof
 )
 
 if not "%1" == "" set BuildMode=%~1
-if not "%~2" == "" set InFile="%~2"
-if not "%~3" == "" set OutFile="%~3"
+if not "%~2" == "" set Platform=%~2
+if not "%~2" == "" set InFile="%~3"
+if not "%~3" == "" set OutFile="%~4"
 
 FOR /F "tokens=1 delims=" %%A in ('git describe --tags --long --dirty') do SET current_tag=%%A
 
+if %Platform%==x64 set BuildMode=%BuildMode%64
+
 echo "%base_dir%\Bin\%BuildMode%\UpdateVersion.exe" %InFile% %OutFile% "%current_tag%"
 "%base_dir%\Bin\%BuildMode%\UpdateVersion.exe" %InFile% %OutFile% "%current_tag%"
-
--- Source/Script/package_zip.cmd
+++ Source/Script/package_zip.cmd
@@ -14,6 +14,8 @@ if "%zip%" == "" (
 
 set ZipFileName=project64
 if not "%1" == "" set ZipFileName=%1
+if "%~2" == "x64" set VSPlatform=64
+
 
 SET current_dir=%cd%
 cd /d %~dp0..\..\
@@ -36,7 +38,7 @@ md "%base_dir%\Bin\Package\Plugin\GFX"
 md "%base_dir%\Bin\Package\Plugin\Input"
 md "%base_dir%\Bin\Package\Plugin\RSP"
 
-copy "%base_dir%\Bin\Release\Project64.exe" "%base_dir%\Bin\Package"
+copy "%base_dir%\Bin\Release%VSPlatform%\Project64.exe" "%base_dir%\Bin\Package"
 copy "%base_dir%\Config\Video.rdb" "%base_dir%\Bin\Package\Config"
 copy "%base_dir%\Config\Audio.rdb" "%base_dir%\Bin\Package\Config"
 copy "%base_dir%\Config\Project64.cht" "%base_dir%\Bin\Package\Config"
@@ -44,12 +46,12 @@ copy "%base_dir%\Config\Project64.enh" "%base_dir%\Bin\Package\Config"
 copy "%base_dir%\Config\Project64.rdb" "%base_dir%\Bin\Package\Config"
 copy "%base_dir%\Config\Project64.rdx" "%base_dir%\Bin\Package\Config"
 copy "%base_dir%\Lang\*.pj.Lang" "%base_dir%\Bin\Package\Lang"
-copy "%base_dir%\Plugin\Audio\Jabo_Dsound.dll" "%base_dir%\Bin\Package\Plugin\Audio"
-copy "%base_dir%\Plugin\Audio\Project64-Audio.dll" "%base_dir%\Bin\Package\Plugin\Audio"
-copy "%base_dir%\Plugin\GFX\Jabo_Direct3D8.dll" "%base_dir%\Bin\Package\Plugin\GFX"
-copy "%base_dir%\Plugin\GFX\Project64-Video.dll" "%base_dir%\Bin\Package\Plugin\GFX"
-copy "%base_dir%\Plugin\Input\PJ64_NRage.dll" "%base_dir%\Bin\Package\Plugin\Input"
-copy "%base_dir%\Plugin\RSP\RSP 1.7.dll" "%base_dir%\Bin\Package\Plugin\RSP"
+copy "%base_dir%\Plugin%VSPlatform%\Audio\Jabo_Dsound.dll" "%base_dir%\Bin\Package\Plugin\Audio"
+copy "%base_dir%\Plugin%VSPlatform%\Audio\Project64-Audio.dll" "%base_dir%\Bin\Package\Plugin\Audio"
+copy "%base_dir%\Plugin%VSPlatform%\GFX\Jabo_Direct3D8.dll" "%base_dir%\Bin\Package\Plugin\GFX"
+copy "%base_dir%\Plugin%VSPlatform%\GFX\Project64-Video.dll" "%base_dir%\Bin\Package\Plugin\GFX"
+copy "%base_dir%\Plugin%VSPlatform%\Input\PJ64_NRage.dll" "%base_dir%\Bin\Package\Plugin\Input"
+copy "%base_dir%\Plugin%VSPlatform%\RSP\RSP 1.7.dll" "%base_dir%\Bin\Package\Plugin\RSP"
 
 cd %base_dir%\Bin\Package
 "%zip%" a -tzip -r "%base_dir%\Package\%ZipFileName%" *
