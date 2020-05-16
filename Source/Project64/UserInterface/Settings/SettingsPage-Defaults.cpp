2be276a7bcab28b415e2c6f701745fd8e1d4940e
oddMLan <oddMLan2@gmail.com>
2020-05-16 06:43:04 -0700

    Tabs -> spaces

--- Source/Project64/UserInterface/Settings/SettingsPage-Defaults.cpp
+++ Source/Project64/UserInterface/Settings/SettingsPage-Defaults.cpp
@@ -20,18 +20,18 @@ CDefaultsOptionsPage::CDefaultsOptionsPage(HWND hParent, const RECT & rcDispay)
     UpdatePageSettings();
 
     SetDlgItemText(IDC_MEMORY_SIZE_TEXT, wGS(ROM_MEM_SIZE).c_str());
-	SetDlgItemText(IDC_MEMORY_SIZE_NOTE, wGS(ROM_MEM_SIZE_NOTE).c_str());
-	SetDlgItemText(IDC_HLE_GFX, wGS(PLUG_HLE_GFX).c_str());
-	SetDlgItemText(IDC_USE_TLB, wGS(ROM_USE_TLB).c_str());
-	SetDlgItemText(IDC_VIREFESH_TEXT, wGS(ROM_VIREFRESH).c_str());
-	SetDlgItemText(IDC_COUNTPERBYTE_TEXT, wGS(ROM_COUNTPERBYTE).c_str());
-	SetDlgItemText(IDC_COUNTFACT_TEXT, wGS(ROM_COUNTER_FACTOR).c_str());
-	SetDlgItemText(IDC_ROM_32BIT, wGS(ROM_32BIT).c_str());
-	SetDlgItemText(IDC_ROM_FIXEDAUDIO, wGS(ROM_FIXED_AUDIO).c_str());
-	SetDlgItemText(IDC_SYNC_AUDIO, wGS(ROM_SYNC_AUDIO).c_str());
-	SetDlgItemText(IDC_UNALIGNED_DMA, wGS(ROM_UNALIGNED_DMA).c_str());
-	SetDlgItemText(IDC_RANDOMIZE_SIPI_INTERRUPTS, wGS(ROM_RANDOMIZE_SIPI_INTERRUPTS).c_str());
-	SetDlgItemText(IDC_PROTECT_MEMORY, wGS(ADVANCE_SMM_PROTECT).c_str());
+    SetDlgItemText(IDC_MEMORY_SIZE_NOTE, wGS(ROM_MEM_SIZE_NOTE).c_str());
+    SetDlgItemText(IDC_HLE_GFX, wGS(PLUG_HLE_GFX).c_str());
+    SetDlgItemText(IDC_USE_TLB, wGS(ROM_USE_TLB).c_str());
+    SetDlgItemText(IDC_VIREFESH_TEXT, wGS(ROM_VIREFRESH).c_str());
+    SetDlgItemText(IDC_COUNTPERBYTE_TEXT, wGS(ROM_COUNTPERBYTE).c_str());
+    SetDlgItemText(IDC_COUNTFACT_TEXT, wGS(ROM_COUNTER_FACTOR).c_str());
+    SetDlgItemText(IDC_ROM_32BIT, wGS(ROM_32BIT).c_str());
+    SetDlgItemText(IDC_ROM_FIXEDAUDIO, wGS(ROM_FIXED_AUDIO).c_str());
+    SetDlgItemText(IDC_SYNC_AUDIO, wGS(ROM_SYNC_AUDIO).c_str());
+    SetDlgItemText(IDC_UNALIGNED_DMA, wGS(ROM_UNALIGNED_DMA).c_str());
+    SetDlgItemText(IDC_RANDOMIZE_SIPI_INTERRUPTS, wGS(ROM_RANDOMIZE_SIPI_INTERRUPTS).c_str());
+    SetDlgItemText(IDC_PROTECT_MEMORY, wGS(ADVANCE_SMM_PROTECT).c_str());
 
     CModifiedComboBox * ComboBox;
     ComboBox = AddModComboBox(GetDlgItem(IDC_RDRAM_SIZE), Default_RDRamSize);
@@ -42,37 +42,37 @@ CDefaultsOptionsPage::CDefaultsOptionsPage(HWND hParent, const RECT & rcDispay)
         ComboBox->AddItem(wGS(RDRAM_8MB).c_str(), 0x800000);
     }
 
-	ComboBox = AddModComboBox(GetDlgItem(IDC_COUNTFACT), Default_CounterFactor);
-	if (ComboBox)
-	{
-		ComboBox->SetTextField(GetDlgItem(IDC_COUNTFACT_TEXT));
-		ComboBox->AddItem(wGS(NUMBER_1).c_str(), 1);
-		ComboBox->AddItem(wGS(NUMBER_2).c_str(), 2);
-		ComboBox->AddItem(wGS(NUMBER_3).c_str(), 3);
-		ComboBox->AddItem(wGS(NUMBER_4).c_str(), 4);
-		ComboBox->AddItem(wGS(NUMBER_5).c_str(), 5);
-		ComboBox->AddItem(wGS(NUMBER_6).c_str(), 6);
-	}
+    ComboBox = AddModComboBox(GetDlgItem(IDC_COUNTFACT), Default_CounterFactor);
+    if (ComboBox)
+    {
+        ComboBox->SetTextField(GetDlgItem(IDC_COUNTFACT_TEXT));
+        ComboBox->AddItem(wGS(NUMBER_1).c_str(), 1);
+        ComboBox->AddItem(wGS(NUMBER_2).c_str(), 2);
+        ComboBox->AddItem(wGS(NUMBER_3).c_str(), 3);
+        ComboBox->AddItem(wGS(NUMBER_4).c_str(), 4);
+        ComboBox->AddItem(wGS(NUMBER_5).c_str(), 5);
+        ComboBox->AddItem(wGS(NUMBER_6).c_str(), 6);
+    }
 
-	CModifiedEditBox * TxtBox = AddModTextBox(GetDlgItem(IDC_VIREFRESH), Default_ViRefreshRate, false);
-	TxtBox->SetTextField(GetDlgItem(IDC_VIREFESH_TEXT));
-	TxtBox = AddModTextBox(GetDlgItem(IDC_COUNTPERBYTE), Default_AiCountPerBytes, false);
-	TxtBox->SetTextField(GetDlgItem(IDC_COUNTPERBYTE_TEXT));
+    CModifiedEditBox * TxtBox = AddModTextBox(GetDlgItem(IDC_VIREFRESH), Default_ViRefreshRate, false);
+    TxtBox->SetTextField(GetDlgItem(IDC_VIREFESH_TEXT));
+    TxtBox = AddModTextBox(GetDlgItem(IDC_COUNTPERBYTE), Default_AiCountPerBytes, false);
+    TxtBox->SetTextField(GetDlgItem(IDC_COUNTPERBYTE_TEXT));
 
-	AddModCheckBox(GetDlgItem(IDC_HLE_GFX), Default_UseHleGfx);
-	AddModCheckBox(GetDlgItem(IDC_USE_TLB), Default_UseTlb);
-	AddModCheckBox(GetDlgItem(IDC_ROM_32BIT), Default_32Bit);
-	AddModCheckBox(GetDlgItem(IDC_SYNC_AUDIO), Default_SyncViaAudio);
-	AddModCheckBox(GetDlgItem(IDC_ROM_FIXEDAUDIO), Default_FixedAudio);
-	AddModCheckBox(GetDlgItem(IDC_UNALIGNED_DMA), Default_UnalignedDMA);
-	AddModCheckBox(GetDlgItem(IDC_RANDOMIZE_SIPI_INTERRUPTS), Default_RandomizeSIPIInterrupts);
-	AddModCheckBox(GetDlgItem(IDC_PROTECT_MEMORY), Default_SMM_Protect_Memory);
+    AddModCheckBox(GetDlgItem(IDC_HLE_GFX), Default_UseHleGfx);
+    AddModCheckBox(GetDlgItem(IDC_USE_TLB), Default_UseTlb);
+    AddModCheckBox(GetDlgItem(IDC_ROM_32BIT), Default_32Bit);
+    AddModCheckBox(GetDlgItem(IDC_SYNC_AUDIO), Default_SyncViaAudio);
+    AddModCheckBox(GetDlgItem(IDC_ROM_FIXEDAUDIO), Default_FixedAudio);
+    AddModCheckBox(GetDlgItem(IDC_UNALIGNED_DMA), Default_UnalignedDMA);
+    AddModCheckBox(GetDlgItem(IDC_RANDOMIZE_SIPI_INTERRUPTS), Default_RandomizeSIPIInterrupts);
+    AddModCheckBox(GetDlgItem(IDC_PROTECT_MEMORY), Default_SMM_Protect_Memory);
 
-	if (!g_Settings->LoadBool(Setting_SyncViaAudioEnabled))
-	{
-		GetDlgItem(IDC_SYNC_AUDIO).EnableWindow(false);
-	}
-	UpdatePageSettings();
+    if (!g_Settings->LoadBool(Setting_SyncViaAudioEnabled))
+    {
+        GetDlgItem(IDC_SYNC_AUDIO).EnableWindow(false);
+    }
+    UpdatePageSettings();
 }
 
 void CDefaultsOptionsPage::HidePage()
