d2b586f9dfb796869d89a80d21a9cb7b77515e2e
zilmar <zilmar@pj64-emu.com>
2021-02-23 15:08:01 +1030

    enable apply button when enabling/disabling enhancements

--- Source/Project64/UserInterface/Settings/SettingsPage-AdvancedOptions.h
+++ Source/Project64/UserInterface/Settings/SettingsPage-AdvancedOptions.h
@@ -11,36 +11,37 @@
 #pragma once
 
 class CAdvancedOptionsPage :
-	public CSettingsPageImpl<CAdvancedOptionsPage>,
-	public CSettingsPage
+    public CSettingsPageImpl<CAdvancedOptionsPage>,
+    public CSettingsPage
 {
 
-	BEGIN_MSG_MAP_EX(CAdvancedOptionsPage)
-		COMMAND_ID_HANDLER_EX(IDC_START_ON_ROM_OPEN,CheckBoxChanged)
-		COMMAND_ID_HANDLER_EX(IDC_ZIP,CheckBoxChanged)
-		COMMAND_ID_HANDLER_EX(IDC_DEBUGGER,CheckBoxChanged)
-		COMMAND_ID_HANDLER_EX(IDC_INTERPRETER, CheckBoxChanged)
-		COMMAND_ID_HANDLER_EX(IDC_REMEMBER_CHEAT,CheckBoxChanged)
-		COMMAND_ID_HANDLER_EX(IDC_CHECK_RUNNING, CheckBoxChanged)
-		COMMAND_ID_HANDLER_EX(IDC_UNIQUE_SAVE_DIR, CheckBoxChanged)
-		COMMAND_ID_HANDLER_EX(IDC_DISPLAY_FRAMERATE, CheckBoxChanged)
-		COMMAND_HANDLER_EX(IDC_FRAME_DISPLAY_TYPE,LBN_SELCHANGE,ComboBoxChanged)
-	END_MSG_MAP()
+    BEGIN_MSG_MAP_EX(CAdvancedOptionsPage)
+        COMMAND_ID_HANDLER_EX(IDC_START_ON_ROM_OPEN,CheckBoxChanged)
+        COMMAND_ID_HANDLER_EX(IDC_ZIP,CheckBoxChanged)
+        COMMAND_ID_HANDLER_EX(IDC_DEBUGGER,CheckBoxChanged)
+        COMMAND_ID_HANDLER_EX(IDC_INTERPRETER, CheckBoxChanged)
+        COMMAND_ID_HANDLER_EX(IDC_REMEMBER_CHEAT,CheckBoxChanged)
+        COMMAND_ID_HANDLER_EX(IDC_CHECK_RUNNING, CheckBoxChanged)
+        COMMAND_ID_HANDLER_EX(IDC_ENABLE_ENHANCEMENTS, CheckBoxChanged)
+        COMMAND_ID_HANDLER_EX(IDC_UNIQUE_SAVE_DIR, CheckBoxChanged)
+        COMMAND_ID_HANDLER_EX(IDC_DISPLAY_FRAMERATE, CheckBoxChanged)
+        COMMAND_HANDLER_EX(IDC_FRAME_DISPLAY_TYPE,LBN_SELCHANGE,ComboBoxChanged)
+    END_MSG_MAP()
 
-	enum { IDD = IDD_Settings_Advanced };
+    enum { IDD = IDD_Settings_Advanced };
 
 public:
-	CAdvancedOptionsPage(HWND hParent, const RECT & rcDispay );
+    CAdvancedOptionsPage(HWND hParent, const RECT & rcDispay );
 
-	LanguageStringID PageTitle ( void ) { return TAB_ADVANCED; }
-	void HidePage ( void );
-	void ShowPage ( void );
-	void ApplySettings ( bool UpdateScreen );
-	bool EnableReset ( void );
-	void ResetPage ( void );
+    LanguageStringID PageTitle ( void ) { return TAB_ADVANCED; }
+    void HidePage ( void );
+    void ShowPage ( void );
+    void ApplySettings ( bool UpdateScreen );
+    bool EnableReset ( void );
+    void ResetPage ( void );
 
 private:
-	void UpdatePageSettings(void);
+    void UpdatePageSettings(void);
 
-	bool m_InUpdateSettings;
+    bool m_InUpdateSettings;
 };
