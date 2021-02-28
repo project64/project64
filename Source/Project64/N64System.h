03dce5f74e186c2a1d37f4381351826a2e1ce42a
Derek "Turtle" Roe <vgturtle127@gmail.com>
2021-02-28 04:28:15 -0600

    Update copyright header for N64System.h
    Switched from block comments to single line comments, updated copyright year, reduced verbosity of license lines, and switched to HTTPS for license link.

--- Source/Project64/N64System.h
+++ Source/Project64/N64System.h
@@ -1,13 +1,8 @@
-/****************************************************************************
-*                                                                           *
-* Project64 - A Nintendo 64 emulator.                                      *
-* http://www.pj64-emu.com/                                                  *
-* Copyright (C) 2012 Project64. All rights reserved.                        *
-*                                                                           *
-* License:                                                                  *
-* GNU/GPLv2 http://www.gnu.org/licenses/gpl-2.0.html                        *
-*                                                                           *
-****************************************************************************/
+// Project64 - A Nintendo 64 emulator
+// http://www.pj64-emu.com/
+// Copyright (C) 2021 Project64. All rights reserved.
+// GNU/GPLv2 licensed: https://gnu.org/licenses/gpl-2.0.html
+
 #pragma once
 
 #include "Support.h"
