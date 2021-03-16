4a62811889c75583d81c07ffdb833ae41967e030
Derek "Turtle" Roe <vgturtle127@gmail.com>
2021-03-16 00:21:03 -0500

    Update common.h
    Remove leftover header, remove title case, reword some comments to sound much better

--- Source/Android/PluginRSP/common.h
+++ Source/Android/PluginRSP/common.h
@@ -1,25 +1,18 @@
-// Project64 - A Nintendo 64 emulator
-// http://www.pj64-emu.com/
-// Copyright(C) 2001-2021 Project64
-// Copyright(C) 2014 Bobby Smiles
-// Copyright(C) 2009 Richard Goedeken
-// Copyright(C) 2002 Hacktarux
-// GNU/GPLv2 licensed: https://gnu.org/licenses/gpl-2.0.html
 #pragma once
 
-/* macro for unused variable warning suppression */
+// Macro for unused variable warning suppression
 #ifdef __GNUC__
 #  define UNUSED(x) UNUSED_ ## x __attribute__((__unused__))
 #else
 #  define UNUSED(x) /* x */
 #endif
 
-/* macro for inline keyword */
+// Macro for inline keyword
 #ifdef _MSC_VER
 #define inline __inline
 #endif
 
-/* Dll function linking */
+// DLL function linking
 #if defined(_WIN32)
 #define EXPORT      extern "C" __declspec(dllexport)
 #define CALL        __cdecl
@@ -28,7 +21,7 @@
 #define CALL
 #endif
 
-/* Plugin types */
+// Plugin types
 enum
 {
     PLUGIN_TYPE_RSP = 1,
@@ -37,17 +30,14 @@ enum
     PLUGIN_TYPE_CONTROLLER = 4,
 };
 
-/***** Structures *****/
-
+// Structures
 typedef struct
 {
-    uint16_t Version;        /* Should be set to 0x0101 */
-    uint16_t Type;           /* Set to PLUGIN_TYPE_RSP */
-    char Name[100];      /* Name of the DLL */
+    uint16_t Version;        // Should be set to 0x0101
+    uint16_t Type;           // Set to PLUGIN_TYPE_RSP
+    char Name[100];      // Name of the DLL
 
-    /* If DLL supports memory these memory options then set them to TRUE or FALSE
-       if it does not support it */
-    int NormalMemory;   /* a normal BYTE array */
-    int MemoryBswaped;  /* a normal BYTE array where the memory has been pre
-                              bswap on a dword (32 bits) boundry */
+    // If DLL supports memory, these memory options then set them to TRUE or FALSE if it does not support it
+    int NormalMemory;   // A normal BYTE array
+    int MemoryBswaped;  // A normal BYTE array where the memory has been pre-bswap'd on a DWORD (32-bits) boundary
 } PLUGIN_INFO;
