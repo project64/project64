cc20cad5908eb5ed2796672cb1ea7f75758d8551
Derek "Turtle" Roe <vgturtle127@gmail.com>
2021-03-16 18:19:15 -0500

    Update md5.cpp
    Convert multi line comments into single line comments, fix typos, fix spacing, fix title case, remove some punctuation, add newline

--- Source/Common/md5.cpp
+++ Source/Common/md5.cpp
@@ -1,43 +1,43 @@
-// MD5.CC - source code for the C++/object oriented translation and
-//          modification of MD5.
+/*
 
-// Translation and modification (c) 1995 by Mordechai T. Abzug
+MD5.CC - source code for the C++/object oriented translation and modification of MD5.
 
-// This translation/ modification is provided "as is," without express or
-// implied warranty of any kind.
+Translation and modification (c) 1995 by Mordechai T. Abzug
 
-// The translator/ modifier does not claim (1) that MD5 will do what you think
-// it does; (2) that this translation/ modification is accurate; or (3) that
-// this software is "merchantible."  (Language for this disclaimer partially
-// copied from the disclaimer below).
+This translation/ modification is provided "as is," without express or
+implied warranty of any kind.
 
-/* based on:
+The translator/ modifier does not claim (1) that MD5 will do what you think
+it does; (2) that this translation/ modification is accurate; or (3) that
+this software is "merchantable."  (Language for this disclaimer partially
+copied from the disclaimer below).
 
-   MD5C.C - RSA Data Security, Inc., MD5 message-digest algorithm
-   MDDRIVER.C - test driver for MD2, MD4 and MD5
+based on:
 
-   Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All
-   rights reserved.
+MD5.H - header file for MD5C.C
+MDDRIVER.C - test driver for MD2, MD4 and MD5
 
-   License to copy and use this software is granted provided that it
-   is identified as the "RSA Data Security, Inc. MD5 Message-Digest
-   Algorithm" in all material mentioning or referencing this software
-   or this function.
+Copyright (C) 1991-2, RSA Data Security, Inc. Created 1991. All rights reserved.
 
-   License is also granted to make and use derivative works provided
-   that such works are identified as "derived from the RSA Data
-   Security, Inc. MD5 Message-Digest Algorithm" in all material
-   mentioning or referencing the derived work.
+License to copy and use this software is granted provided that it
+is identified as the "RSA Data Security, Inc. MD5 Message-Digest
+Algorithm" in all material mentioning or referencing this software
+or this function.
 
-   RSA Data Security, Inc. makes no representations concerning either
-   the merchantability of this software or the suitability of this
-   software for any particular purpose. It is provided "as is"
-   without express or implied warranty of any kind.
+License is also granted to make and use derivative works provided
+that such works are identified as "derived from the RSA Data
+Security, Inc. MD5 Message-Digest Algorithm" in all material
+mentioning or referencing the derived work.
 
-   These notices must be retained in any copies of any part of this
-   documentation and/or software.
+RSA Data Security, Inc. makes no representations concerning either
+the merchantability of this software or the suitability of this
+software for any particular purpose. It is provided "as is"
+without express or implied warranty of any kind.
 
-   */
+These notices must be retained in any copies of any part of this
+documentation and/or software.
+
+*/
 
 #include "stdafx.h"
 
@@ -54,12 +54,13 @@ MD5::~MD5()
 // MD5 block update operation. Continues an MD5 message-digest
 // operation, processing another message block, and updating the
 // context.
+
 void MD5::update(const uint1 *input, uint4 input_length)
 {
     uint4 input_index, buffer_index;
-    uint4 buffer_space;                // how much space is left in buffer
+    uint4 buffer_space;                // How much space is left in the buffer
 
-    if (finalized) // so we can't update!
+    if (finalized) // So we can't update!
     {
         WriteTrace(TraceMD5, TraceError, "Can't update a finalized digest!");
         return;
@@ -76,34 +77,34 @@ void MD5::update(const uint1 *input, uint4 input_length)
 
     count[1] += ((uint4)input_length >> 29);
 
-    buffer_space = 64 - buffer_index;  // how much space is left in buffer
+    buffer_space = 64 - buffer_index;  // How much space is left in the buffer
 
-    // Transform as many times as possible.
-    if (input_length >= buffer_space) // ie. we have enough to fill the buffer
+    // Transform as many times as possible
+    if (input_length >= buffer_space) // i.e. we have enough to fill the buffer
     {
-        // fill the rest of the buffer and transform
+        // Fill the rest of the buffer and transform
         memcpy(buffer + buffer_index, (unsigned char *)input, buffer_space);
         transform(buffer);
 
-        // now, transform each 64-byte piece of the input, bypassing the buffer
+        // Now, transform each 64-byte piece of the input, bypassing the buffer
         for (input_index = buffer_space; input_index + 63 < input_length; input_index += 64)
         {
             transform((unsigned char *)(input + input_index));
         }
 
-        buffer_index = 0; // so we can buffer remaining
+        buffer_index = 0; // So we can buffer remaining
     }
     else
     {
-        input_index = 0; // so we can buffer the whole input
+        input_index = 0; // So we can buffer the whole input
     }
 
-    // and here we do the buffering:
+    // Here we do the buffering:
     memcpy(buffer + buffer_index, (unsigned char *)(input + input_index), input_length - input_index);
 }
 
-// MD5 update for files.
-// Like above, except that it works on files (and uses above as a primitive.)
+// MD5 update for files
+// Like above, except that it works on files (and uses above as a primitive)
 
 void MD5::update(FILE *file)
 {
@@ -123,7 +124,7 @@ void MD5::update(FILE *file)
 }
 
 // MD5 finalization. Ends an MD5 message-digest operation, writing the
-// the message digest and zeroizing the context.
+// the message digest and zeroing the context
 
 void MD5::finalize()
 {
@@ -145,7 +146,7 @@ void MD5::finalize()
     // Save number of bits
     encode(bits, count, 8);
 
-    // Pad out to 56 mod 64.
+    // Pad out to 56 mod 64
     index = (uint4)((count[0] >> 3) & 0x3f);
     padLen = (index < 56) ? (56 - index) : (120 - index);
     update(PADDING, padLen);
@@ -164,7 +165,7 @@ void MD5::finalize()
 
 MD5::MD5(CPath File)
 {
-    init();  // must be called be all constructors
+    init();  // Must be called by all constructors
     if (File.Exists())
     {
         FILE * fp = fopen((const char *)File, "rb");
@@ -178,21 +179,21 @@ MD5::MD5(CPath File)
 
 MD5::MD5(FILE *file)
 {
-    init();  // must be called be all constructors
+    init();  // Must be called by all constructors
     update(file);
     finalize();
 }
 
 MD5::MD5(const unsigned char *input, unsigned int input_length)
 {
-    init();  // must called by all constructors
+    init();  // Must be called by all constructors
     update(input, input_length);
     finalize();
 }
 
 MD5::MD5(const stdstr & string)
 {
-    init();  // must called by all constructors
+    init();  // Must be called by all constructors
     update((const unsigned char *)string.c_str(), string.length());
     finalize();
 }
@@ -245,15 +246,16 @@ const char *MD5::hex_digest()
 }
 
 // PRIVATE METHODS:
+
 void MD5::init()
 {
-    finalized = 0;  // we just started!
+    finalized = 0;  // We just started!
 
     // Nothing counted, so count=0
     count[0] = 0;
     count[1] = 0;
 
-    // Load magic initialization constants.
+    // Load magic initialization constants
     state[0] = 0x67452301;
     state[1] = 0xefcdab89;
     state[2] = 0x98badcfe;
@@ -287,97 +289,99 @@ void MD5::init()
 #define S44 21
 
 // MD5 basic transformation. Transforms state based on block.
+
 void MD5::transform(uint1 block[64])
 {
     uint4 a = state[0], b = state[1], c = state[2], d = state[3], x[16];
 
     decode(x, block, 64);
 
-    //ATLASSERT(!finalized);  // not just a user error, since the method is private
+    //ATLASSERT(!finalized);  // Not just a user error, since the method is private
 
-    /* Round 1 */
-    FF(a, b, c, d, x[0], S11, 0xd76aa478); /* 1 */
-    FF(d, a, b, c, x[1], S12, 0xe8c7b756); /* 2 */
-    FF(c, d, a, b, x[2], S13, 0x242070db); /* 3 */
-    FF(b, c, d, a, x[3], S14, 0xc1bdceee); /* 4 */
-    FF(a, b, c, d, x[4], S11, 0xf57c0faf); /* 5 */
-    FF(d, a, b, c, x[5], S12, 0x4787c62a); /* 6 */
-    FF(c, d, a, b, x[6], S13, 0xa8304613); /* 7 */
-    FF(b, c, d, a, x[7], S14, 0xfd469501); /* 8 */
-    FF(a, b, c, d, x[8], S11, 0x698098d8); /* 9 */
-    FF(d, a, b, c, x[9], S12, 0x8b44f7af); /* 10 */
-    FF(c, d, a, b, x[10], S13, 0xffff5bb1); /* 11 */
-    FF(b, c, d, a, x[11], S14, 0x895cd7be); /* 12 */
-    FF(a, b, c, d, x[12], S11, 0x6b901122); /* 13 */
-    FF(d, a, b, c, x[13], S12, 0xfd987193); /* 14 */
-    FF(c, d, a, b, x[14], S13, 0xa679438e); /* 15 */
-    FF(b, c, d, a, x[15], S14, 0x49b40821); /* 16 */
+    // Round 1
+    FF(a, b, c, d, x[0], S11, 0xd76aa478); // 1
+    FF(d, a, b, c, x[1], S12, 0xe8c7b756); // 2
+    FF(c, d, a, b, x[2], S13, 0x242070db); // 3
+    FF(b, c, d, a, x[3], S14, 0xc1bdceee); // 4
+    FF(a, b, c, d, x[4], S11, 0xf57c0faf); // 5
+    FF(d, a, b, c, x[5], S12, 0x4787c62a); // 6
+    FF(c, d, a, b, x[6], S13, 0xa8304613); // 7
+    FF(b, c, d, a, x[7], S14, 0xfd469501); // 8
+    FF(a, b, c, d, x[8], S11, 0x698098d8); // 9
+    FF(d, a, b, c, x[9], S12, 0x8b44f7af); // 10
+    FF(c, d, a, b, x[10], S13, 0xffff5bb1); // 11
+    FF(b, c, d, a, x[11], S14, 0x895cd7be); // 12
+    FF(a, b, c, d, x[12], S11, 0x6b901122); // 13
+    FF(d, a, b, c, x[13], S12, 0xfd987193); // 14
+    FF(c, d, a, b, x[14], S13, 0xa679438e); // 15
+    FF(b, c, d, a, x[15], S14, 0x49b40821); // 16
 
-    /* Round 2 */
-    GG(a, b, c, d, x[1], S21, 0xf61e2562); /* 17 */
-    GG(d, a, b, c, x[6], S22, 0xc040b340); /* 18 */
-    GG(c, d, a, b, x[11], S23, 0x265e5a51); /* 19 */
-    GG(b, c, d, a, x[0], S24, 0xe9b6c7aa); /* 20 */
-    GG(a, b, c, d, x[5], S21, 0xd62f105d); /* 21 */
-    GG(d, a, b, c, x[10], S22, 0x2441453); /* 22 */
-    GG(c, d, a, b, x[15], S23, 0xd8a1e681); /* 23 */
-    GG(b, c, d, a, x[4], S24, 0xe7d3fbc8); /* 24 */
-    GG(a, b, c, d, x[9], S21, 0x21e1cde6); /* 25 */
-    GG(d, a, b, c, x[14], S22, 0xc33707d6); /* 26 */
-    GG(c, d, a, b, x[3], S23, 0xf4d50d87); /* 27 */
-    GG(b, c, d, a, x[8], S24, 0x455a14ed); /* 28 */
-    GG(a, b, c, d, x[13], S21, 0xa9e3e905); /* 29 */
-    GG(d, a, b, c, x[2], S22, 0xfcefa3f8); /* 30 */
-    GG(c, d, a, b, x[7], S23, 0x676f02d9); /* 31 */
-    GG(b, c, d, a, x[12], S24, 0x8d2a4c8a); /* 32 */
+    // Round 2
+    GG(a, b, c, d, x[1], S21, 0xf61e2562); // 17
+    GG(d, a, b, c, x[6], S22, 0xc040b340); // 18
+    GG(c, d, a, b, x[11], S23, 0x265e5a51); // 19
+    GG(b, c, d, a, x[0], S24, 0xe9b6c7aa); // 20
+    GG(a, b, c, d, x[5], S21, 0xd62f105d); // 21
+    GG(d, a, b, c, x[10], S22, 0x2441453); // 22
+    GG(c, d, a, b, x[15], S23, 0xd8a1e681); // 23
+    GG(b, c, d, a, x[4], S24, 0xe7d3fbc8); // 24
+    GG(a, b, c, d, x[9], S21, 0x21e1cde6); // 25
+    GG(d, a, b, c, x[14], S22, 0xc33707d6); // 26
+    GG(c, d, a, b, x[3], S23, 0xf4d50d87); // 27
+    GG(b, c, d, a, x[8], S24, 0x455a14ed); // 28
+    GG(a, b, c, d, x[13], S21, 0xa9e3e905); // 29
+    GG(d, a, b, c, x[2], S22, 0xfcefa3f8); // 30
+    GG(c, d, a, b, x[7], S23, 0x676f02d9); // 31
+    GG(b, c, d, a, x[12], S24, 0x8d2a4c8a); // 32
 
-    /* Round 3 */
-    HH(a, b, c, d, x[5], S31, 0xfffa3942); /* 33 */
-    HH(d, a, b, c, x[8], S32, 0x8771f681); /* 34 */
-    HH(c, d, a, b, x[11], S33, 0x6d9d6122); /* 35 */
-    HH(b, c, d, a, x[14], S34, 0xfde5380c); /* 36 */
-    HH(a, b, c, d, x[1], S31, 0xa4beea44); /* 37 */
-    HH(d, a, b, c, x[4], S32, 0x4bdecfa9); /* 38 */
-    HH(c, d, a, b, x[7], S33, 0xf6bb4b60); /* 39 */
-    HH(b, c, d, a, x[10], S34, 0xbebfbc70); /* 40 */
-    HH(a, b, c, d, x[13], S31, 0x289b7ec6); /* 41 */
-    HH(d, a, b, c, x[0], S32, 0xeaa127fa); /* 42 */
-    HH(c, d, a, b, x[3], S33, 0xd4ef3085); /* 43 */
-    HH(b, c, d, a, x[6], S34, 0x4881d05); /* 44 */
-    HH(a, b, c, d, x[9], S31, 0xd9d4d039); /* 45 */
-    HH(d, a, b, c, x[12], S32, 0xe6db99e5); /* 46 */
-    HH(c, d, a, b, x[15], S33, 0x1fa27cf8); /* 47 */
-    HH(b, c, d, a, x[2], S34, 0xc4ac5665); /* 48 */
+    // Round 3
+    HH(a, b, c, d, x[5], S31, 0xfffa3942); // 33
+    HH(d, a, b, c, x[8], S32, 0x8771f681); // 34
+    HH(c, d, a, b, x[11], S33, 0x6d9d6122); // 35
+    HH(b, c, d, a, x[14], S34, 0xfde5380c); // 36
+    HH(a, b, c, d, x[1], S31, 0xa4beea44); // 37
+    HH(d, a, b, c, x[4], S32, 0x4bdecfa9); // 38
+    HH(c, d, a, b, x[7], S33, 0xf6bb4b60); // 39
+    HH(b, c, d, a, x[10], S34, 0xbebfbc70); // 40
+    HH(a, b, c, d, x[13], S31, 0x289b7ec6); // 41
+    HH(d, a, b, c, x[0], S32, 0xeaa127fa); // 42
+    HH(c, d, a, b, x[3], S33, 0xd4ef3085); // 43
+    HH(b, c, d, a, x[6], S34, 0x4881d05); // 44
+    HH(a, b, c, d, x[9], S31, 0xd9d4d039); // 45
+    HH(d, a, b, c, x[12], S32, 0xe6db99e5); // 46
+    HH(c, d, a, b, x[15], S33, 0x1fa27cf8); // 47
+    HH(b, c, d, a, x[2], S34, 0xc4ac5665); // 48
 
-    /* Round 4 */
-    II(a, b, c, d, x[0], S41, 0xf4292244); /* 49 */
-    II(d, a, b, c, x[7], S42, 0x432aff97); /* 50 */
-    II(c, d, a, b, x[14], S43, 0xab9423a7); /* 51 */
-    II(b, c, d, a, x[5], S44, 0xfc93a039); /* 52 */
-    II(a, b, c, d, x[12], S41, 0x655b59c3); /* 53 */
-    II(d, a, b, c, x[3], S42, 0x8f0ccc92); /* 54 */
-    II(c, d, a, b, x[10], S43, 0xffeff47d); /* 55 */
-    II(b, c, d, a, x[1], S44, 0x85845dd1); /* 56 */
-    II(a, b, c, d, x[8], S41, 0x6fa87e4f); /* 57 */
-    II(d, a, b, c, x[15], S42, 0xfe2ce6e0); /* 58 */
-    II(c, d, a, b, x[6], S43, 0xa3014314); /* 59 */
-    II(b, c, d, a, x[13], S44, 0x4e0811a1); /* 60 */
-    II(a, b, c, d, x[4], S41, 0xf7537e82); /* 61 */
-    II(d, a, b, c, x[11], S42, 0xbd3af235); /* 62 */
-    II(c, d, a, b, x[2], S43, 0x2ad7d2bb); /* 63 */
-    II(b, c, d, a, x[9], S44, 0xeb86d391); /* 64 */
+    // Round 4
+    II(a, b, c, d, x[0], S41, 0xf4292244); // 49
+    II(d, a, b, c, x[7], S42, 0x432aff97); // 50
+    II(c, d, a, b, x[14], S43, 0xab9423a7); // 51
+    II(b, c, d, a, x[5], S44, 0xfc93a039); // 52
+    II(a, b, c, d, x[12], S41, 0x655b59c3); // 53
+    II(d, a, b, c, x[3], S42, 0x8f0ccc92); // 54
+    II(c, d, a, b, x[10], S43, 0xffeff47d); // 55
+    II(b, c, d, a, x[1], S44, 0x85845dd1); // 56
+    II(a, b, c, d, x[8], S41, 0x6fa87e4f); // 57
+    II(d, a, b, c, x[15], S42, 0xfe2ce6e0); // 58
+    II(c, d, a, b, x[6], S43, 0xa3014314); // 59
+    II(b, c, d, a, x[13], S44, 0x4e0811a1); // 60
+    II(a, b, c, d, x[4], S41, 0xf7537e82); // 61
+    II(d, a, b, c, x[11], S42, 0xbd3af235); // 62
+    II(c, d, a, b, x[2], S43, 0x2ad7d2bb); // 63
+    II(b, c, d, a, x[9], S44, 0xeb86d391); // 64
 
     state[0] += a;
     state[1] += b;
     state[2] += c;
     state[3] += d;
 
-    // Zeroize sensitive information.
+    // Zeroize sensitive information
     memset((uint1 *)x, 0, sizeof(x));
 }
 
 // Encodes input (UINT4) into output (unsigned char). Assumes len is
 // a multiple of 4.
+
 void MD5::encode(uint1 *output, uint4 *input, uint4 len)
 {
     unsigned int i, j;
@@ -393,6 +397,7 @@ void MD5::encode(uint1 *output, uint4 *input, uint4 len)
 
 // Decodes input (unsigned char) into output (UINT4). Assumes len is
 // a multiple of 4.
+
 void MD5::decode(uint4 *output, uint1 *input, uint4 len)
 {
     unsigned int i, j;
@@ -403,7 +408,8 @@ void MD5::decode(uint4 *output, uint1 *input, uint4 len)
     }
 }
 
-// Note: Replace "for loop" with standard memcpy if possible.
+// Note: Replace "for loop" with standard memcpy if possible
+
 void MD5::memcpy(uint1 *output, uint1 *input, uint4 len)
 {
     unsigned int i;
@@ -414,7 +420,8 @@ void MD5::memcpy(uint1 *output, uint1 *input, uint4 len)
     }
 }
 
-// Note: Replace "for loop" with standard memset if possible.
+// Note: Replace "for loop" with standard memset if possible
+
 void MD5::memset(uint1 *output, uint1 value, uint4 len)
 {
     unsigned int i;
@@ -425,14 +432,14 @@ void MD5::memset(uint1 *output, uint1 value, uint4 len)
     }
 }
 
-// ROTATE_LEFT rotates x left n bits.
+// ROTATE_LEFT rotates x left n bits
 
 inline unsigned int MD5::rotate_left(uint4 x, uint4 n)
 {
     return (x << n) | (x >> (32 - n));
 }
 
-// F, G, H and I are basic MD5 functions.
+// F, G, H and I are basic MD5 functions
 
 inline unsigned int MD5::F(uint4 x, uint4 y, uint4 z)
 {
