4458dc3375a73e1d1a7150ac5d859ea9bd290f6f
Derek "Turtle" Roe <vgturtle127@gmail.com>
2021-04-02 02:34:27 -0500

    Update Notification.h
    Fix comments

--- Source/Project64-core/Notification.h
+++ Source/Project64-core/Notification.h
@@ -9,14 +9,14 @@
 __interface CNotification
 {
 public:
-    //Error Messages
+    // Error messages
     virtual void DisplayError(const char * Message) const = 0;
     virtual void DisplayError(LanguageStringID StringID) const = 0;
 
     virtual void FatalError(const char * Message) const = 0;
     virtual void FatalError(LanguageStringID StringID) const = 0;
 
-    //User Feedback
+    // User feedback
     virtual void DisplayWarning(const char * Message) const = 0;
     virtual void DisplayWarning(LanguageStringID StringID) const = 0;
 
@@ -24,7 +24,7 @@ public:
     virtual void DisplayMessage(int DisplayTime, LanguageStringID StringID) const = 0;
     virtual void DisplayMessage2(const char * Message) const = 0;
 
-    // Ask a Yes/No Question to the user, yes = true, no = false
+    // Ask a yes/no question to the user, yes = true, no = false
     virtual bool AskYesNoQuestion(const char * Question) const = 0;
 
     virtual void BreakPoint(const char * FileName, int32_t LineNumber) = 0;
