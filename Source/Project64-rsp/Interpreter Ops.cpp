fbb388fa0f2d818ce5293e866504a7c5fbdb7078
zilmar <zilmar@pj64-emu.com>
2023-07-06 20:51:17 +0930

    Rsp: Fix capitalization in rsp_UnknownOpcode

--- Source/Project64-rsp/Interpreter Ops.cpp
+++ Source/Project64-rsp/Interpreter Ops.cpp
@@ -2249,7 +2249,7 @@ void rsp_UnknownOpcode(void)
     }
     else
     {
-        sprintf(Message, "Unhandled Opcode\n%s\n\nStopping emulation.\n\nWOuld you like to open the debugger?",
+        sprintf(Message, "Unhandled Opcode\n%s\n\nStopping emulation.\n\nWould you like to open the debugger?",
             RSPInstruction(*PrgCount, RSPOpC.Value).NameAndParam().c_str());
         response = MessageBoxA(NULL, Message, "Error", MB_YESNO | MB_ICONERROR);
         if (response == IDYES)
