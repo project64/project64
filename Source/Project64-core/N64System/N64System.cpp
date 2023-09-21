aadcca7528feb2fe5a41ad34e553a3441e6f0f4d
zilmar <zilmar@pj64-emu.com>
2023-09-21 18:40:27 +0930

    Core: Fix clang issue

--- Source/Project64-core/N64System/N64System.cpp
+++ Source/Project64-core/N64System/N64System.cpp
@@ -1540,7 +1540,7 @@ void CN64System::DumpSyncErrors(CN64System * SecondCPU)
         for (count = 0; count < 32; count++)
         {
             Error.LogF("CP0[%s] %*s0x%08X%08X, 0x%08X%08X\r\n", CRegName::Cop0[count], 12 - strlen(CRegName::Cop0[count]), "",
-                (uint32_t)(m_Reg.m_CP0[count] >> 32), (uint32_t)m_Reg.m_CP0[count], (uint32_t)(SecondCPU->m_Reg.m_CP0[count] >> 32), (uint32_t)(SecondCPU->m_Reg.m_CP0[count]));
+                       (uint32_t)(m_Reg.m_CP0[count] >> 32), (uint32_t)m_Reg.m_CP0[count], (uint32_t)(SecondCPU->m_Reg.m_CP0[count] >> 32), (uint32_t)(SecondCPU->m_Reg.m_CP0[count]));
         }
         Error.Log("\r\n");
         for (count = 0; count < 32; count++)
