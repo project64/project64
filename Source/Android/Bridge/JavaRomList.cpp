d3f41327707b38430eb76e60288e5c07e99af0fd
zilmar <zilmar@pj64-emu.com>
2023-10-26 11:18:24 +1030

    Android: When listing a rom not in rdb, use game file instead

--- Source/Android/Bridge/JavaRomList.cpp
+++ Source/Android/Bridge/JavaRomList.cpp
@@ -1,5 +1,6 @@
 #include "JavaRomList.h"
 #include "JavaBridge.h"
+#include <Project64-core/Multilanguage.h>
 
 #ifdef ANDROID
 extern JavaBridge * g_JavaBridge;
@@ -17,6 +18,11 @@ void CJavaRomList::RomAddedToList(int32_t ListPos)
     if (g_JavaBridge)
     {
         ROM_INFO * pRomInfo = &m_RomInfo[ListPos];
+        if (pRomInfo->GoodName[0] == '#' && strcmp("#340#", pRomInfo->GoodName) == 0 && g_Lang != nullptr)
+        {
+            std::string GoodName = g_Lang->GetString(RB_NOT_GOOD_FILE);
+            strncpy(pRomInfo->GoodName, GoodName.c_str(), sizeof(pRomInfo->GoodName) / sizeof(char));
+        }
         g_JavaBridge->RomListAddItem(pRomInfo->szFullFileName, pRomInfo->FileName, pRomInfo->GoodName, pRomInfo->TextColor);
     }
 }
