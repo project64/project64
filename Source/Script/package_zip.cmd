02e816b9d40e8e5403f5bcf9f9c0ef9d5d86ac78
zilmar <zilmar@pj64-emu.com>
2024-09-05 19:22:25 +0930

    Update package_zip.cmd to deal with platform in path

--- Source/Script/package_zip.cmd
+++ Source/Script/package_zip.cmd
@@ -13,9 +13,9 @@ if "%zip%" == "" (
 )
 
 set ZipFileName=project64
+set VSPlatform=win32
 if not "%1" == "" set ZipFileName=%1
-if "%~2" == "x64" set VSPlatform=64
-
+if not "%2" == "" set VSPlatform=%2
 
 SET current_dir=%cd%
 cd /d %~dp0..\..\
@@ -33,21 +33,21 @@ md "%base_dir%\Bin\Package\Config"
 md "%base_dir%\Bin\Package\Config\Cheats"
 md "%base_dir%\Bin\Package\Config\Enhancements"
 md "%base_dir%\Bin\Package\Lang"
-md "%base_dir%\Bin\Package\Plugin%VSPlatform%"
-md "%base_dir%\Bin\Package\Plugin%VSPlatform%\Audio"
-md "%base_dir%\Bin\Package\Plugin%VSPlatform%\GFX"
-md "%base_dir%\Bin\Package\Plugin%VSPlatform%\Input"
-md "%base_dir%\Bin\Package\Plugin%VSPlatform%\RSP"
+md "%base_dir%\Bin\Package\Plugin\%VSPlatform%"
+md "%base_dir%\Bin\Package\Plugin\%VSPlatform%\Audio"
+md "%base_dir%\Bin\Package\Plugin\%VSPlatform%\GFX"
+md "%base_dir%\Bin\Package\Plugin\%VSPlatform%\Input"
+md "%base_dir%\Bin\Package\Plugin\%VSPlatform%\RSP"
 
-IF EXIST "%base_dir%\Plugin%VSPlatform%\GFX\GLideN64" (
-md "%base_dir%\Bin\Package\Plugin%VSPlatform%\GFX\GLideN64"
-copy "%base_dir%\Plugin%VSPlatform%\GFX\GLideN64\GLideN64.dll" "%base_dir%\Bin\Package\Plugin%VSPlatform%\GFX\GLideN64\GLideN64.dll"
-copy "%base_dir%\Plugin%VSPlatform%\GFX\GLideN64\*.ini" "%base_dir%\Bin\Package\Plugin%VSPlatform%\GFX\GLideN64"
-md "%base_dir%\Bin\Package\Plugin%VSPlatform%\GFX\GLideN64\translations"
-copy "%base_dir%\Plugin%VSPlatform%\GFX\GLideN64\translations\*.Lang" "%base_dir%\Bin\Package\Plugin%VSPlatform%\GFX\GLideN64\translations"
+IF EXIST "%base_dir%\Plugin\%VSPlatform%\GFX\GLideN64" (
+md "%base_dir%\Bin\Package\Plugin\%VSPlatform%\GFX\GLideN64"
+copy "%base_dir%\Plugin\%VSPlatform%\GFX\GLideN64\GLideN64.dll" "%base_dir%\Bin\Package\Plugin\%VSPlatform%\GFX\GLideN64\GLideN64.dll"
+copy "%base_dir%\Plugin\%VSPlatform%\GFX\GLideN64\*.ini" "%base_dir%\Bin\Package\Plugin\%VSPlatform%\GFX\GLideN64"
+md "%base_dir%\Bin\Package\Plugin\%VSPlatform%\GFX\GLideN64\translations"
+copy "%base_dir%\Plugin\%VSPlatform%\GFX\GLideN64\translations\*.Lang" "%base_dir%\Bin\Package\Plugin\%VSPlatform%\GFX\GLideN64\translations"
 )
 
-copy "%base_dir%\Bin\Release%VSPlatform%\Project64.exe" "%base_dir%\Bin\Package"
+copy "%base_dir%\Bin\%VSPlatform%\Release\Project64.exe" "%base_dir%\Bin\Package"
 copy "%base_dir%\Config\Video.rdb" "%base_dir%\Bin\Package\Config"
 copy "%base_dir%\Config\Audio.rdb" "%base_dir%\Bin\Package\Config"
 copy "%base_dir%\Config\Cheats\*.cht" "%base_dir%\Bin\Package\Config\Cheats"
@@ -55,13 +55,13 @@ copy "%base_dir%\Config\Enhancements\*.enh" "%base_dir%\Bin\Package\Config\Enhan
 copy "%base_dir%\Config\Project64.rdb" "%base_dir%\Bin\Package\Config"
 copy "%base_dir%\Config\Project64.rdx" "%base_dir%\Bin\Package\Config"
 copy "%base_dir%\Lang\*.pj.Lang" "%base_dir%\Bin\Package\Lang"
-copy "%base_dir%\Plugin%VSPlatform%\Audio\Jabo_Dsound.dll" "%base_dir%\Bin\Package\Plugin%VSPlatform%\Audio"
-copy "%base_dir%\Plugin%VSPlatform%\Audio\Project64-Audio.dll" "%base_dir%\Bin\Package\Plugin%VSPlatform%\Audio"
-copy "%base_dir%\Plugin%VSPlatform%\GFX\Jabo_Direct3D8.dll" "%base_dir%\Bin\Package\Plugin%VSPlatform%\GFX"
-copy "%base_dir%\Plugin%VSPlatform%\GFX\Project64-Video.dll" "%base_dir%\Bin\Package\Plugin%VSPlatform%\GFX"
-copy "%base_dir%\Plugin%VSPlatform%\Input\PJ64_NRage.dll" "%base_dir%\Bin\Package\Plugin%VSPlatform%\Input"
-copy "%base_dir%\Plugin%VSPlatform%\Input\Project64-Input.dll" "%base_dir%\Bin\Package\Plugin%VSPlatform%\Input"
-copy "%base_dir%\Plugin%VSPlatform%\RSP\Project64-RSP.dll" "%base_dir%\Bin\Package\Plugin%VSPlatform%\RSP"
+copy "%base_dir%\Plugin\%VSPlatform%\Audio\Jabo_Dsound.dll" "%base_dir%\Bin\Package\Plugin\%VSPlatform%\Audio"
+copy "%base_dir%\Plugin\%VSPlatform%\Audio\Project64-Audio.dll" "%base_dir%\Bin\Package\Plugin\%VSPlatform%\Audio"
+copy "%base_dir%\Plugin\%VSPlatform%\GFX\Jabo_Direct3D8.dll" "%base_dir%\Bin\Package\Plugin\%VSPlatform%\GFX"
+copy "%base_dir%\Plugin\%VSPlatform%\GFX\Project64-Video.dll" "%base_dir%\Bin\Package\Plugin\%VSPlatform%\GFX"
+copy "%base_dir%\Plugin\%VSPlatform%\Input\PJ64_NRage.dll" "%base_dir%\Bin\Package\Plugin\%VSPlatform%\Input"
+copy "%base_dir%\Plugin\%VSPlatform%\Input\Project64-Input.dll" "%base_dir%\Bin\Package\Plugin\%VSPlatform%\Input"
+copy "%base_dir%\Plugin\%VSPlatform%\RSP\Project64-RSP.dll" "%base_dir%\Bin\Package\Plugin\%VSPlatform%\RSP"
 
 cd %base_dir%\Bin\Package
 "%zip%" a -tzip -r "%base_dir%\Package\%ZipFileName%" *
