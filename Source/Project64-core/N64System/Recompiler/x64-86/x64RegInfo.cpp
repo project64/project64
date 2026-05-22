10d25bfdeb07ae3a41082e73bfb8fd40edc6c2e4
zilmar <zilmar@pj64-emu.com>
2026-05-22 09:51:39 +0930

    Core: Add ability for x64 recompiler to be able map gpr 32bit registers

--- Source/Project64-core/N64System/Recompiler/x64-86/x64RegInfo.cpp
+++ Source/Project64-core/N64System/Recompiler/x64-86/x64RegInfo.cpp
@@ -1,13 +1,120 @@
 #include "stdafx.h"
 #if defined(__amd64__) || defined(_M_X64)
 
+#include <Project64-core/N64System/Mips/Register.h>
+#include <Project64-core/N64System/Recompiler/CodeBlock.h>
 #include <Project64-core/N64System/Recompiler/x64-86/x64RegInfo.h>
 
-CX64RegInfo::CX64RegInfo(CCodeBlock & /*CodeBlock*/, CX64Ops & /*Assembler*/)
+namespace
 {
+asmjit::x86::Gp GetX64RegFromPhysId(uint32_t PhysId, asmjit::RegType RegType = asmjit::RegType::kX86_Gpq)
+{
+    using namespace asmjit::x86;
+
+    if (PhysId > Gp::kIdR15)
+    {
+        g_Notify->BreakPoint(__FILE__, __LINE__);
+        return asmjit::x86::Gp();
+    }
+
+    if (RegType == asmjit::RegType::kX86_Gpd)
+    {
+        return Gpd(PhysId);
+    }
+    if (RegType == asmjit::RegType::kX86_Gpq)
+    {
+        return Gpq(PhysId);
+    }
+
+    g_Notify->BreakPoint(__FILE__, __LINE__);
+    return asmjit::x86::Gp();
 }
 
-CX64RegInfo::CX64RegInfo(const CX64RegInfo &)
+static const char * X64GpName(const asmjit::x86::Gp & Reg)
+{
+    asmjit::RegType width = asmjit::RegType::kX86_Gpq;
+    if (Reg.isType(asmjit::RegType::kX86_Gpd))
+    {
+        width = asmjit::RegType::kX86_Gpd;
+    }
+    else if (!Reg.isType(asmjit::RegType::kX86_Gpq))
+    {
+        g_Notify->BreakPoint(__FILE__, __LINE__);
+    }
+
+    static const char * const kName64[] = {
+        "rax",
+        "rcx",
+        "rdx",
+        "rbx",
+        "rsp",
+        "rbp",
+        "rsi",
+        "rdi",
+        "r8",
+        "r9",
+        "r10",
+        "r11",
+        "r12",
+        "r13",
+        "r14",
+        "r15",
+    };
+    static const char * const kName32[] = {
+        "eax",
+        "ecx",
+        "edx",
+        "ebx",
+        "esp",
+        "ebp",
+        "esi",
+        "edi",
+        "r8d",
+        "r9d",
+        "r10d",
+        "r11d",
+        "r12d",
+        "r13d",
+        "r14d",
+        "r15d",
+    };
+
+    const uint32_t id = Reg.id();
+    if (id <= asmjit::x86::Gp::kIdR15)
+    {
+        return width == asmjit::RegType::kX86_Gpd ? kName32[id] : kName64[id];
+    }
+
+    g_Notify->BreakPoint(__FILE__, __LINE__);
+    return "?";
+}
+} // namespace
+
+CX64RegInfo::CX64RegInfo(CCodeBlock & CodeBlock, CX64Ops & Assembler) :
+    m_Reg(CodeBlock.Registers()),
+    m_CodeBlock(CodeBlock),
+    m_Assembler(Assembler),
+    m_InBeforeCallDirect(false),
+    m_CallDirectPushBase(0),
+    m_CallDirectPushCount(0)
+{
+    for (int32_t i = 0; i < 32; i++)
+    {
+        m_RegMap[i] = asmjit::x86::Gp();
+    }
+    for (int32_t i = 0; i < x64PhysRegCount; i++)
+    {
+        m_x64reg_MappedTo[i] = NotMapped;
+        m_x64reg_Protected[i] = false;
+        m_x64reg_MapOrder[i] = 0;
+    }
+    memset(&m_CallDirectPushIds, 0, sizeof(m_CallDirectPushIds));
+}
+
+CX64RegInfo::CX64RegInfo(const CX64RegInfo & rhs) :
+    m_Reg(rhs.m_Reg),
+    m_CodeBlock(rhs.m_CodeBlock),
+    m_Assembler(rhs.m_Assembler)
 {
     g_Notify->BreakPoint(__FILE__, __LINE__);
 }
@@ -19,6 +126,16 @@ CX64RegInfo::~CX64RegInfo()
 CX64RegInfo & CX64RegInfo::operator=(const CX64RegInfo & right)
 {
     CRegBase::operator=(right);
+
+    memcpy(&m_RegMap, &right.m_RegMap, sizeof(m_RegMap));
+    memcpy(&m_x64reg_MappedTo, &right.m_x64reg_MappedTo, sizeof(m_x64reg_MappedTo));
+    memcpy(&m_x64reg_Protected, &right.m_x64reg_Protected, sizeof(m_x64reg_Protected));
+    memcpy(&m_x64reg_MapOrder, &right.m_x64reg_MapOrder, sizeof(m_x64reg_MapOrder));
+    m_InBeforeCallDirect = right.m_InBeforeCallDirect;
+    m_CallDirectPushBase = right.m_CallDirectPushBase;
+    m_CallDirectPushCount = right.m_CallDirectPushCount;
+    memcpy(&m_CallDirectPushIds, &right.m_CallDirectPushIds, sizeof(m_CallDirectPushIds));
+
 #ifdef _DEBUG
     if (*this != right)
     {
@@ -35,6 +152,21 @@ bool CX64RegInfo::operator==(const CX64RegInfo & Right) const
         return false;
     }
 
+    for (int32_t i = 0; i < x64PhysRegCount; i++)
+    {
+        if (m_x64reg_MappedTo[i] != Right.m_x64reg_MappedTo[i])
+        {
+            return false;
+        }
+        if (m_x64reg_Protected[i] != Right.m_x64reg_Protected[i])
+        {
+            return false;
+        }
+        if (m_x64reg_MapOrder[i] != Right.m_x64reg_MapOrder[i])
+        {
+            return false;
+        }
+    }
     return true;
 }
 
@@ -43,8 +175,173 @@ bool CX64RegInfo::operator!=(const CX64RegInfo & Right) const
     return !(Right == *this);
 }
 
+const asmjit::x86::Gp & CX64RegInfo::GetMipsRegMap(int32_t Reg) const
+{
+    return m_RegMap[Reg];
+}
+
+void CX64RegInfo::SetMipsRegMap(int32_t MipsReg, const asmjit::x86::Gp & Reg)
+{
+    m_RegMap[MipsReg] = Reg;
+}
+
+bool CX64RegInfo::GetX64Protected(uint32_t PhysId) const
+{
+    return m_x64reg_Protected[PhysId];
+}
+
+CX64RegInfo::REG_MAPPED CX64RegInfo::GetX64Mapped(uint32_t PhysId) const
+{
+    return m_x64reg_MappedTo[PhysId];
+}
+
+void CX64RegInfo::SetX64MapOrder(uint32_t PhysId, uint32_t Order)
+{
+    m_x64reg_MapOrder[PhysId] = Order;
+}
+
+void CX64RegInfo::SetX64Protected(uint32_t PhysId, bool Protected)
+{
+    m_x64reg_Protected[PhysId] = Protected;
+}
+
+void CX64RegInfo::SetX64Mapped(uint32_t PhysId, REG_MAPPED Mapping)
+{
+    m_x64reg_MappedTo[PhysId] = Mapping;
+}
+
 void CX64RegInfo::ResetRegisterProtection()
 {
+    for (uint32_t k = 0; k < x64PhysRegCount; k++)
+    {
+        m_x64reg_Protected[k] = false;
+    }
+}
+
+void CX64RegInfo::BeforeCallDirect(void)
+{
+    // Win64 integer argument / return registers (caller-saved).
+    static constexpr uint32_t kX64CallClobberRegIds[] = {
+        asmjit::x86::Gp::kIdAx,
+        asmjit::x86::Gp::kIdCx,
+        asmjit::x86::Gp::kIdDx,
+        asmjit::x86::Gp::kIdR8,
+        asmjit::x86::Gp::kIdR9,
+        asmjit::x86::Gp::kIdR10,
+        asmjit::x86::Gp::kIdR11,
+    };
+    static constexpr uint32_t kX64CallClobberRegCount = sizeof(kX64CallClobberRegIds) / sizeof(kX64CallClobberRegIds[0]);
+
+    if (m_InBeforeCallDirect)
+    {
+        g_Notify->BreakPoint(__FILE__, __LINE__);
+        return;
+    }
+    m_InBeforeCallDirect = true;
+    m_CallDirectPushBase = m_CallDirectPushCount;
+
+    for (uint32_t k = 0; k < kX64CallClobberRegCount; k++)
+    {
+        const uint32_t physId = kX64CallClobberRegIds[k];
+        if (GetX64Mapped(physId) == NotMapped)
+        {
+            continue;
+        }
+        if (m_CallDirectPushCount >= sizeof(m_CallDirectPushIds) / sizeof(m_CallDirectPushIds[0]))
+        {
+            g_Notify->BreakPoint(__FILE__, __LINE__);
+            return;
+        }
+        const asmjit::x86::Gp Reg = GetX64RegFromPhysId(physId, asmjit::RegType::kX86_Gpq);
+        m_CodeBlock.Log("    regcache: push %s for call", X64GpName(Reg));
+        m_Assembler.push(Reg);
+        m_CallDirectPushIds[m_CallDirectPushCount++] = physId;
+    }
+}
+
+void CX64RegInfo::AfterCallDirect(void)
+{
+    if (!m_InBeforeCallDirect)
+    {
+        g_Notify->BreakPoint(__FILE__, __LINE__);
+        return;
+    }
+
+    while (m_CallDirectPushCount > m_CallDirectPushBase)
+    {
+        const uint32_t physId = m_CallDirectPushIds[--m_CallDirectPushCount];
+        const asmjit::x86::Gp Reg = GetX64RegFromPhysId(physId, asmjit::RegType::kX86_Gpq);
+        m_CodeBlock.Log("    regcache: pop %s after call", X64GpName(Reg));
+        m_Assembler.pop(Reg);
+    }
+
+    m_InBeforeCallDirect = m_CallDirectPushCount != 0;
+}
+
+void CX64RegInfo::Map_GPR_32bit(int32_t MipsReg, bool SignValue, int32_t MipsRegToLoad)
+{
+    if (MipsReg == 0)
+    {
+        g_Notify->BreakPoint(__FILE__, __LINE__);
+        return;
+    }
+
+    asmjit::x86::Gp Reg;
+    if (IsUnknown(MipsReg) || IsConst(MipsReg))
+    {
+        Reg = FreeX64Reg(asmjit::RegType::kX86_Gpd);
+        if (Reg.isNone())
+        {
+            if (g_DebugSettings.haveDebugger)
+            {
+                g_Notify->DisplayError("Map_GPR_32bit\n\nOut of registers");
+            }
+            g_Notify->BreakPoint(__FILE__, __LINE__);
+            return;
+        }
+        m_CodeBlock.Log("    regcache: allocate %s to %s", X64GpName(Reg), CRegName::GPR[MipsReg]);
+    }
+    else
+    {
+        g_Notify->BreakPoint(__FILE__, __LINE__);
+        return;
+    }
+    for (int i = 0; i < x64PhysRegCount; i++)
+    {
+        if (m_x64reg_MapOrder[i] > 0)
+        {
+            m_x64reg_MapOrder[i] += 1;
+        }
+    }
+    const uint32_t RegIndex = Reg.id();
+    SetX64MapOrder(RegIndex, 1);
+
+    if (MipsRegToLoad > 0)
+    {
+        if (IsUnknown(MipsRegToLoad))
+        {
+            m_Assembler.mov(Reg.r32(), asmjit::x86::dword_ptr((uintptr_t)&m_Reg.m_GPR[MipsRegToLoad].UW[0]));
+        }
+        else if (IsMapped(MipsRegToLoad))
+        {
+            if (MipsReg != MipsRegToLoad)
+            {
+                m_Assembler.mov(Reg.r32(), GetMipsRegMap(MipsRegToLoad).r32());
+            }
+        }
+        else
+        {
+            m_Assembler.mov(Reg.r32(), (uint32_t)GetMipsRegLo(MipsRegToLoad));
+        }
+    }
+    else if (MipsRegToLoad == 0)
+    {
+        m_Assembler.xor_(Reg.r32(), Reg.r32());
+    }
+    SetX64Mapped(RegIndex, GPR_Mapped);
+    SetX64Protected(RegIndex, true);
+    SetMipsRegMap(MipsReg, Reg);
+    SetMipsRegState(MipsReg, SignValue ? STATE_MAPPED_32_SIGN : STATE_MAPPED_32_ZERO);
 }
 
 void CX64RegInfo::WriteBackRegisters()
@@ -76,6 +373,42 @@ void CX64RegInfo::UnMap_GPR(uint32_t Reg, bool WriteBackValue)
     }
 
     g_Notify->BreakPoint(__FILE__, __LINE__);
+}
+
+asmjit::x86::Gp CX64RegInfo::FreeX64Reg(asmjit::RegType RegType)
+{
+    static constexpr uint32_t kX64AllocatableRegIds[] = {
+        asmjit::x86::Gp::kIdBx,
+        asmjit::x86::Gp::kIdR12,
+        asmjit::x86::Gp::kIdR13,
+        asmjit::x86::Gp::kIdR14,
+        asmjit::x86::Gp::kIdR15,
+        asmjit::x86::Gp::kIdDi,
+        asmjit::x86::Gp::kIdSi,
+        asmjit::x86::Gp::kIdBp,
+        asmjit::x86::Gp::kIdAx,
+        asmjit::x86::Gp::kIdCx,
+        asmjit::x86::Gp::kIdDx,
+        asmjit::x86::Gp::kIdR8,
+        asmjit::x86::Gp::kIdR9,
+        asmjit::x86::Gp::kIdR10,
+        asmjit::x86::Gp::kIdR11,
+    };
+    static constexpr uint32_t kX64AllocatableRegCount = sizeof(kX64AllocatableRegIds) / sizeof(kX64AllocatableRegIds[0]);
+
+    for (uint32_t k = 0; k < kX64AllocatableRegCount; k++)
+    {
+        const uint32_t physId = kX64AllocatableRegIds[k];
+        if (GetX64Mapped(physId) == NotMapped && !GetX64Protected(physId))
+        {
+            if (RegType == asmjit::RegType::kX86_Gpd)
+            {
+                return asmjit::x86::Gpq(kX64AllocatableRegIds[k]);
+            }
+            return GetX64RegFromPhysId(physId, RegType);
+        }
+    }
+    return asmjit::x86::Gp();
 }
 
 #endif
--- Source/Project64-core/N64System/Recompiler/x64-86/x64RegInfo.h
+++ Source/Project64-core/N64System/Recompiler/x64-86/x64RegInfo.h
@@ -2,14 +2,27 @@
 
 #if defined(__amd64__) || defined(_M_X64)
 #include <Project64-core/N64System/Recompiler/RegBase.h>
+#include <Project64-core/N64System/Recompiler/asmjit.h>
 
 class CCodeBlock;
 class CX64Ops;
+class CRegisters;
 
 class CX64RegInfo :
     public CRegBase
 {
+    enum
+    {
+        x64PhysRegCount = asmjit::x86::Gp::kIdR15 + 1
+    };
+
 public:
+    enum REG_MAPPED
+    {
+        NotMapped,
+        GPR_Mapped,
+    };
+
     CX64RegInfo(CCodeBlock & CodeBlock, CX64Ops & Assembler);
     CX64RegInfo(const CX64RegInfo &);
     ~CX64RegInfo();
@@ -20,8 +33,37 @@ public:
     bool operator!=(const CX64RegInfo & right) const;
 
     void ResetRegisterProtection();
+    void BeforeCallDirect(void);
+    void AfterCallDirect(void);
+    void Map_GPR_32bit(int32_t MipsReg, bool SignValue, int32_t MipsRegToLoad);
+    const asmjit::x86::Gp & GetMipsRegMap(int32_t Reg) const;
+    void SetMipsRegMap(int32_t MipsReg, const asmjit::x86::Gp & Reg);
+
+    bool GetX64Protected(uint32_t PhysId) const;
+    REG_MAPPED GetX64Mapped(uint32_t PhysId) const;
+    void SetX64MapOrder(uint32_t PhysId, uint32_t Order);
+    void SetX64Protected(uint32_t PhysId, bool Protected);
+    void SetX64Mapped(uint32_t PhysId, REG_MAPPED Mapping);
+
     void UnMap_GPR(uint32_t Reg, bool WriteBackValue);
     void WriteBackRegisters();
+
+private:
+    CX64RegInfo() = delete;
+
+    asmjit::x86::Gp FreeX64Reg(asmjit::RegType RegType);
+
+    CRegisters & m_Reg;
+    CCodeBlock & m_CodeBlock;
+    CX64Ops & m_Assembler;
+    asmjit::x86::Gp m_RegMap[32];
+    REG_MAPPED m_x64reg_MappedTo[x64PhysRegCount];
+    uint32_t m_x64reg_MapOrder[x64PhysRegCount];
+    bool m_x64reg_Protected[x64PhysRegCount];
+    bool m_InBeforeCallDirect;
+    uint32_t m_CallDirectPushBase;
+    uint32_t m_CallDirectPushCount;
+    uint32_t m_CallDirectPushIds[7];
 };
 
 #endif
